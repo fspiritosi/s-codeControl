2e57132ff249e6f8e6241cfce77a33abf54ea9bb
Diego Castillo <92400440+Dac1977@users.noreply.github.com>
2024-01-22 20:04:05 -0300

    modificación de formulario2

--- src/components/CompanyRegister.tsx
+++ src/components/CompanyRegister.tsx
@@ -25,10 +25,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from '@/components/ui/select'
-<<<<<<< HEAD
-=======
-import { supabase } from '@/supabase/supabase'
->>>>>>> 226860e (modificación de formulario)
+import { supabase } from '../../supabase/supabase'
 
 export function CompanyRegister() {
   const { insertCompany, fetchProvinces, fetchCities, provinces, cities } =
