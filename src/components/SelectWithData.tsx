531f777692d69f99a50a1111f84bcc0f9eccc9fb
Yordan <yordanpz@hotmail.com>
2024-01-25 18:06:57 -0300

    Refactor SelectWithData component to handle options of type Province

--- src/components/SelectWithData.tsx
+++ src/components/SelectWithData.tsx
@@ -7,24 +7,39 @@ import {
 } from '@/components/ui/select'
 import { Label } from './ui/label'
 
+type Province = {
+  id: number
+  name: string
+}
 type Props = {
-  label: string
+  label?: string
   placeholder: string
-  options: string[] | undefined
+  options: string[] | undefined | Province[]
+  onChange: (value: string) => void
+  value: string
 }
 
-export const SelectWithData = ({ label, placeholder, options }: Props) => {
+export const SelectWithData = ({ label, placeholder, options, onChange, value  }: Props) => {
+  let dataToRender = options
+
+  if ((options?.[0] as Province)?.name) {
+    dataToRender = (options as Province[]).map(
+      (option: Province) => option.name,
+    )
+  }
   return (
     <>
-      <Label className='ml-2' htmlFor={label}>{label}</Label>
-      <Select required>
-        <SelectTrigger className="">
+      <Label className="ml-2" htmlFor={label}>
+        {label}
+      </Label>
+      <Select required value={value} onValueChange={onChange}>
+        <SelectTrigger>
           <SelectValue placeholder={placeholder} />
         </SelectTrigger>
-        <SelectContent >
-          {options?.map((option, index) => (
-            <SelectItem key={index} value={option}>
-              {option}
+        <SelectContent>
+          {dataToRender?.map((option, index) => (
+            <SelectItem key={index} value={String(option)}>
+              {String(option)}
             </SelectItem>
           ))}
         </SelectContent>
