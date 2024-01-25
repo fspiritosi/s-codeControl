76d4e1fa4ea038ef189ab418a18c3e97e1f66f22
Yordan <yordanpz@hotmail.com>
2024-01-25 18:07:08 -0300

    Update Sidebar width in Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,7 +3,7 @@ import SideLinks from './SideLinks'
 
 export default function SideBar() {
   return (
-    <div className="flex h-full flex-col px-3 py-4 md:px-2  w-1/6">
+    <div className="flex h-full flex-col px-3 py-4 md:px-2 w-1/5">
       <Link
         className="mb-2 flex h-20 items-end justify-start rounded-md bg-gray-400 p-4 md:h-40"
         href="/"
