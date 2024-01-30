8e8cafa560e368be1544a08b228c900c4a861479
Yordan <yordanpz@hotmail.com>
2024-01-30 18:32:20 -0300

    Add console.log statement to log imageFile

--- src/components/UploadImage.tsx
+++ src/components/UploadImage.tsx
@@ -35,6 +35,7 @@ export function UploadImage({
   const [base64Image, setBase64Image] = useState<string>('')
   const [disabled, setDisabled] = useState<boolean>(false)
   const { toast } = useToast()
+  console.log(imageFile)
 
   const handleImageChange = (event: ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0]
