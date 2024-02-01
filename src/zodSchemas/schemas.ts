8160983a349d80c7061b0ecce337693242366993
Yordan <yordanpz@hotmail.com>
2024-02-01 18:08:25 -0300

    Add validation and error messages to companySchema and accordionSchema

--- src/zodSchemas/schemas.ts
+++ src/zodSchemas/schemas.ts
@@ -1,4 +1,43 @@
 import * as z from 'zod'
+import { supabase } from '../../supabase/supabase'
+
+const getAllFiles = async (legajo:string) => {
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser()
+
+  const profile = await supabase
+  .from('profile')
+  .select('*')
+  .eq('credential_id', user?.id)
+
+  // console.log(profile.data?.[0].id)
+
+  const {data} = await supabase.
+  from('company')
+  .select('*')
+  .eq('owner_id',profile.data?.[0].id)
+
+
+  const { data: employee } = await supabase
+    .from('companies_employees')
+    .select('*')
+    .eq('company_id', data?.[0].id)
+    .eq('file', legajo)
+
+
+    if (employee && employee.length > 0) {
+      return false
+    } else {
+      return true
+    }
+
+}
+
+
+
+// const AllFiles = 
 
 const passwordSchema = z
   .string()
@@ -103,88 +142,100 @@ export const companySchema = z.object({
     
   city: z.number(),
   company_logo: z.string().optional(),
-  employees: z.string().nullable(),
+  employees_id: z.string().nullable(),
 })
 
 export const accordionSchema = z.object({
+  full_name: z.string().min(2, {}).optional(),
   lastname: z.string().min(2, {
-    message: 'El nombre debe tener al menos 2 caracteres.',
-  }),
-  firstname: z.string().min(2, {
     message: 'El apellido debe tener al menos 2 caracteres.',
   }),
-  nationality: z.enum(['Argentino', 'Extranjero'],{
-    invalid_type_error: 'Nacionalidad invalida'
+  firstname: z.string().min(2, {
+    message: 'El nombre debe tener al menos 2 caracteres.',
   }),
-  cuil: z
-  .string()
-  .refine(
+  nationality: z.string({
+    required_error: "La nacionalidad es requerida",
+  }),
+  cuil: z.string().refine(
     value => value.length === 13 && value[2] === '-' && value[11] === '-',
     {
       message: 'El CUIT se debe ingresar con el formato xx-xxxxxxxx-x',
     },
   ),
-  document_type: z.enum(['DNI', 'PASAPORTE','LC','LE'],{
-    invalid_type_error: 'Tipo de documento invalido'
+  document_type: z.string({
+    required_error: "El tipo de documento es requerido"
   }),
-  document_number: z
-  .string()
-  .min(7, {
+  document_number: z.string().min(7, {
     message: 'El documento debe tener al menos 7 caracteres.',
   }).max(10, {
     message: 'El documento debe tener menos de 11 caracteres.',
   }),
-  birthplace: z.string(),
-  gender: z.enum(['Masculino', 'Femenino', 'Otro'],{
-    invalid_type_error: 'Genero invalido'
+  birthplace: z.string({
+    required_error: "El lugar de nacimiento es requerido"
   }),
-  marital_status: z.enum(['Soltero', 'Casado', 'Divorciado', 'Viudo'],{
-    invalid_type_error: 'Estado civil invalido'
+  gender: z.string({
+    required_error: "El género es requerido"
   }),
-  level_of_education: z.enum(['Primario', 'Secundario', 'Terciario', 'Universitario','Posgrado'],{
-    invalid_type_error: 'Nivel de educacion invalido'
+  marital_status: z.string({
+    required_error: "El estado civil es requerido"
   }),
-  picture: z.string().refine((value) => {
-    return value.trim().length > 0;
-  }, {
-    message: 'Se debe seleccionar la foto y subirla.',
+  level_of_education: z.string({
+    required_error: "El nivel de educación es requerido"
   }),
-  street : z.string().min(2, {
+  picture: z.string().optional(),
+  street: z.string().min(2, {
     message: 'La calle debe tener al menos 2 caracteres.',
- }),
- street_number : z.string().min(1, {
+  }),
+  street_number: z.string().min(1, {
     message: 'El número debe tener al menos 1 caracteres.',
- }),
- province: z.string({ description: 'La provincia es requerida.'}),
- city: z.string(),
- postal_code: z.string().min(4, {
+  }),
+  province: z.string({
+    required_error: "La provincia es requerida"
+  }),
+  city: z.string({
+    required_error: "La ciudad es requerida"
+  }),
+  postal_code: z.string().min(4, {
     message: 'El código postal debe tener al menos 4 caracteres.',
- })
- ,
- phone : z.string().min(4, {
+  }),
+  phone: z.string().min(4, {
     message: 'El teléfono debe tener al menos 4 caracteres.',
- }).max(15, {
+  }).max(15, {
     message: 'El teléfono debe tener menos de 15 caracteres.',
- }),
- email: z.string().email(),
- file: z.string().max(10,{message:"El legajo no debe tener mas de 10 caracteres"}).min(1),
- hierarchical_position: z.enum(['Director', 'Supervisor', 'Gerente','Operativo','Administrativo'],{
-  invalid_type_error: 'Posicion jerarquica invalida'
- }),
- company_position: z.string().min(3, {
-  message: 'El cargo debe tener al menos 3 caracteres.',
- }),
- workflow_diagram: z.string().refine((value) => value.trim().length > 0, {
-  message: 'El archivo no puede estar vacío.',
- }),
- normal_hours: z.string().refine((value) => value.trim().length > 0, {
-  message: 'El archivo no puede estar vacío.',
- }),
- type_of_contract: z.enum(['Período de prueba', 'A tiempo indeterminado', 'Plazo fijo'],{
-  invalid_type_error: 'Tipo de contrato invalido'
- }),
- allocated_to: z.string().min(3, {
-  message: 'El cargo debe tener al menos 3 caracteres.',
- }),
- date_of_admission: z.string(),
+  }),
+  email: z.string().email({
+    message: 'Email inválido'
+  }).optional(),
+  file: z.string({
+    description: 'El número de legajo es obligatorio'
+  })
+    .regex(/^[0-9]+$/, {
+      message: 'No se pueden ingresar valores negativos ni símbolos'
+    })
+    .max(10, {
+      message: "El legajo no debe tener más de 10 caracteres"
+    })
+    .min(1, {
+      message: 'El legajo debe contener al menos un número'
+    })
+    .refine(async (value) => {
+      return await getAllFiles(value)
+    }, {
+      message: "El legajo ya existe"
+    }),
+  hierarchical_position: z.string({
+    required_error: "El cargo jerárquico es requerido"
+  }),
+  company_position: z.string().min(3, {
+    message: 'El cargo debe tener al menos 3 caracteres.',
+  }),
+  workflow_diagram: z.string({required_error: "El diagrama de flujo es requerido"}),
+  normal_hours: z.string().refine((value) => value.trim().length > 0, {
+    message: 'El archivo no puede estar vacío.',
+  }),
+  type_of_contract: z.string({required_error: "El tipo de contrato es requerido"}),
+  allocated_to: z.array(z.string()),
+  date_of_admission: z.date({
+    required_error: "La fecha de ingreso es requerida",
+  }).optional(),
 })
