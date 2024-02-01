05cf8521fba8e953e996ffe3184ce78c98a71351
Yordan <yordanpz@hotmail.com>
2024-02-01 18:23:14 -0300

    Refactor Popover component

--- src/components/ui/popover.tsx
+++ src/components/ui/popover.tsx
@@ -1,18 +1,9 @@
-<<<<<<< HEAD
 "use client"
 
 import * as React from "react"
 import * as PopoverPrimitive from "@radix-ui/react-popover"
 
 import { cn } from "@/lib/utils"
-=======
-'use client'
-
-import * as React from 'react'
-import * as PopoverPrimitive from '@radix-ui/react-popover'
-
-import { cn } from '@/lib/utils'
->>>>>>> aa3f94bfd496199475e832837343d86c61bca2e4
 
 const Popover = PopoverPrimitive.Root
 
@@ -23,24 +14,15 @@ const PopoverAnchor = PopoverPrimitive.Anchor
 const PopoverContent = React.forwardRef<
   React.ElementRef<typeof PopoverPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof PopoverPrimitive.Content>
-<<<<<<< HEAD
 >(({ className, align = "center", sideOffset = 4, ...props }, ref) => (
-=======
->(({ className, align = 'center', sideOffset = 4, ...props }, ref) => (
->>>>>>> aa3f94bfd496199475e832837343d86c61bca2e4
   <PopoverPrimitive.Portal>
     <PopoverPrimitive.Content
       ref={ref}
       align={align}
       sideOffset={sideOffset}
       className={cn(
-<<<<<<< HEAD
         "z-50 w-72 rounded-md border bg-popover p-4 text-popover-foreground shadow-md outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
         className
-=======
-        'z-50 w-72 rounded-md border bg-popover p-4 text-popover-foreground shadow-md outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
-        className,
->>>>>>> aa3f94bfd496199475e832837343d86c61bca2e4
       )}
       {...props}
     />
