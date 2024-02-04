36d778c403e42ef96f69451c633a67ee499d1ef8
Yordan <yordanpz@hotmail.com>
2024-02-04 12:35:47 -0300

    Agrega datos de empleados a la página de empleados

--- src/app/dashboard/employee/page.tsx
+++ src/app/dashboard/employee/page.tsx
@@ -5,6 +5,8 @@ import { DataTable } from './data-table'
 
 const EmployeePage = () => {
   //Crear un arreglo de empleados
+  const data2 = ['YPF', 'ARSA']
+  const data3 = ['YPF', 'COTO']
   const data = [
     {
       full_name: 'Juan Perez',
@@ -15,7 +17,38 @@ const EmployeePage = () => {
       company_position: 'Empleado',
       normal_hours: '8',
       type_of_contract: 'A tiempo indeterminado',
-      allocated_to: 'Empresa 1',
+      allocated_to: data2.map((value, index) => index > 0 ? ` ${value}` : value),
+      picture: 'picture.com',
+      nationality: 'Argentina',
+      lastname: 'Perez',
+      firstname: 'Juan',
+      document_type: 'DNI',
+      birthplace: 'Buenos Aires',
+      gender: 'Masculino',
+      marital_status: 'Soltero',
+      level_of_education: 'Universitario',
+      street: 'Calle 123',
+      street_number: '123',
+      province: 'Buenos Aires',
+      postal_code: '1234',
+      phone: '12345678',
+      file: '123',
+      date_of_admission: '01/01/2021',
+      affiliate_status: 'Fuera de convenio',
+      city: 'Buenos Aires',
+      hierrical_position: 'Empleado',
+      workflow_diagram: '123',
+    },
+    {
+      full_name: 'Juan Perez',
+      email: 'empleado1@hotmail.com',
+      cuil: '20-12345678-9',
+      document_number: '12345678',
+      hierarchical_position: 'Empleado',
+      company_position: 'Empleado',
+      normal_hours: '8',
+      type_of_contract: 'A tiempo indeterminado',
+      allocated_to: data3.map((value, index) => index > 0 ? ` ${value}` : value),
       picture: 'picture.com',
       nationality: 'Argentina',
       lastname: 'Perez',
