69bd219b0021e256748a1a31e1f34413f4762a80
Yordan <yordanpz@hotmail.com>
2024-02-05 04:11:01 -0300

    Actualizar useEmployeesData.ts con cambios en la creación y actualización de empleados

--- src/hooks/useEmployeesData.ts
+++ src/hooks/useEmployeesData.ts
@@ -1,26 +1,39 @@
-import { Employee } from '@/types/types';
-import { supabase } from '../../supabase/supabase';
-import { useEdgeFunctions } from './useEdgeFunctions';
-import { useLoggedUserStore } from '@/store/loggedUser';
-
+import { useLoggedUserStore } from '@/store/loggedUser'
+import { Employee } from '@/types/types'
+import { supabase } from '../../supabase/supabase'
+import { useEdgeFunctions } from './useEdgeFunctions'
 
 export const useEmployeesData = () => {
-    const {errorTranslate} = useEdgeFunctions()
-    const company = useLoggedUserStore(state => state.actualCompany)
-    
-return{
-    createEmployee: async (employee: Employee) => {
-        const { data, error } = await supabase
-        .from('employees')
-        .insert({...employee,
-            company_id: company?.id,
-        })
+  const { errorTranslate } = useEdgeFunctions()
+  const company = useLoggedUserStore(state => state.actualCompany)
 
-        if (error) {
-            const message = await errorTranslate(error.message)
-            throw new Error(String(message).replaceAll('"', ''))
-          }
-        return data
-    }
+  return {
+    createEmployee: async (employee: Employee) => {
+      const { data, error } = await supabase
+        .from('employees')
+        .insert({ ...employee, company_id: company?.id })
+
+      if (error) {
+        const message = await errorTranslate(error.message)
+        throw new Error(String(message).replaceAll('"', ''))
+      }
+      return data
+    },
+    updateEmployee: async (employee: Employee) => {
+      console.log(employee, 'employee');
+      const { data, error } = await supabase
+        .from('employees')
+        .update(employee)
+        .eq('document_number', employee?.document_number)
+        .select()
+        
+        console.log(data);
+        
+      if (error) {
+        const message = await errorTranslate(error.message)
+        throw new Error(String(message).replaceAll('"', ''))
+      }
+      return data
+    },
+  }
 }
-};
