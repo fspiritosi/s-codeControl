71531a503b1c5e7b2bff14f68c78374b687ef961
Diego Castillo <92400440+Dac1977@users.noreply.github.com>
2024-02-17 08:11:03 -0300

    Update page.tsx
    conectada Tabla data-equipment a la DB

--- src/app/dashboard/equipment/page.tsx
+++ src/app/dashboard/equipment/page.tsx
@@ -4,7 +4,8 @@ import { useLoggedUserStore } from '@/store/loggedUser'
 import Link from 'next/link'
 import { columns } from './columns'
 import { DataEquipment } from './data-equipment'
-
+import { supabase } from '../../../../supabase/supabase'
+import { useEffect, useState } from 'react'
 const pruebaArray = [
   {
     picture:
@@ -58,13 +59,46 @@ const pruebaArray = [
 ]
 
 export default function Equipment() {
-  //const employees = useLoggedUserStore(state => state.employees)
+  const [vehiclesData, setVehiclesData] = useState<unknown[]>([])
+
+  const fetchVehicles = async () => {
+    try {
+      const { data: vehicles, error } = await supabase
+        .from('vehicles')
+        .select('*')
+      if (error) {
+        console.error('Error al obtener los vehículos:', error)
+      } else {
+        setVehiclesData(vehicles)
+      }
+    } catch (error) {
+      console.error('Error al obtener los vehículos:', error)
+    }
+  }
+
+  useEffect(() => {
+    fetchVehicles()
+  }, [])
+
+  useEffect(() => {
+    const channels = supabase
+      .channel('custom-all-channel')
+      .on(
+        'postgres_changes',
+        { event: '*', schema: 'public', table: 'vehicles' },
+        payload => {
+          fetchVehicles()
+        },
+      )
+      .subscribe()
+  }, [])
+
   return (
     <main className="bg-white">
       <h2 className="text-3xl pb-5 pl-10">Todos los Equipos</h2>
       <p className="pl-10 max-w-1/2">Aquí se verán todos los Equipos</p>
       <div className="flex flex-col items-center justify-center min-h-screen py-2">
-        <DataEquipment columns={columns} data={pruebaArray || []} />
+        <DataEquipment columns={columns} data={vehiclesData || []} />
       </div>
     </main>
   )
