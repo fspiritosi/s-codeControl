4e02007b7acac5489c6f09e70764d72e30a36bc2
Yordan <yordanpz@hotmail.com>
2024-02-17 02:11:02 -0300

    Agregado componente Modal con soporte para agregar marcas y modelos

---
+++ src/components/Modal.tsx
@@ -0,0 +1,29 @@
+import AddBrandModal from './AddBrandModal'
+import AddModelModal from './AddModelModal'
+
+export function Modal({
+  children,
+  modal,
+  fetchData,
+  brandOptions,
+  fetchModels,
+}: {
+  children: React.ReactNode
+  modal: string
+  fetchData?: () => Promise<void>
+  brandOptions?: { label: string; id: string }[]
+  fetchModels?: (brand_id: string) => Promise<void>
+}) {
+  return (
+    <>
+      {modal === 'addBrand' && fetchData && (
+        <AddBrandModal fetchData={fetchData}>{children}</AddBrandModal>
+      )}
+      {modal === 'addModel' && brandOptions && (
+        <AddModelModal fetchModels={fetchModels} brandOptions={brandOptions}>
+          {children}
+        </AddModelModal>
+      )}
+    </>
+  )
+}
