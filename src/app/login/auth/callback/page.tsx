b894a88ddc52784e3ade853268092d02e55aab3b
Yordan <yordanpz@hotmail.com>
2024-03-02 00:43:46 -0300

    Añadida importación de Skeleton y ajustes de estilo en la página de autenticación

--- src/app/login/auth/callback/page.tsx
+++ src/app/login/auth/callback/page.tsx
@@ -1,10 +1,9 @@
 'use client'
+import { Skeleton } from '@/components/ui/skeleton'
 import { useEdgeFunctions } from '@/hooks/useEdgeFunctions'
 import { useRouter } from 'next/navigation'
 import { useEffect } from 'react'
 import { supabase } from '../../../../../supabase/supabase'
-import { Skeleton } from "@/components/ui/skeleton"
-
 
 export default function Callback() {
   const router = useRouter()
@@ -28,26 +27,33 @@ export default function Callback() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
+  const size = `calc(100vh - 442px)`
+
   return (
-    <main className="flex min-h-screen">
-      <aside className="w-1/5 px-4 py-5 box-border">
+    <main
+      className="flex min-h-screen max-h-[90vh]"
+      style={{ maxHeight: '100vh', padding: '10px' }}
+    >
+      <aside
+        className="w-1/5 px-4 box-border"
+        style={{ maxHeight: '95vh', maxWidth: 250 }}
+      >
         {/* Sidebar */}
         <Skeleton className="h-[95vh] w-full " />
       </aside>
-      <div className="flex flex-col w-full">
-        <nav className=" p-4">
+      <div className="flex flex-col w-full" style={{ height: '100%' }}>
+        <nav className="mb-4">
           {/* Navbar */}
           <Skeleton className="h-10 w-full " />
         </nav>
-        <section className="p-4">
+        <section className="" style={{ maxHeight: '100%', height: '100%' }}>
           {/* Employee Table */}
-          <Skeleton className="h-96 w-full " />
-          <Skeleton className="h-4 mt-4" />
+          <Skeleton className="h-72 w-full " />
           <Skeleton className="h-4 mt-2" />
           <Skeleton className="h-4 mt-2" />
-          <Skeleton className="h-4 mt-2" />
-          <Skeleton className="h-52 w-full mt-2" />
-          <Skeleton className="h-52 w-full mt-2" />
+          {/* <Skeleton className="h-52 w-full mt-2" /> */}
+          <Skeleton className="w-full mt-2" style={{ height: size }} />
+          {/* <Skeleton className=" h-full w-full mt-2 pb-4" /> */}
         </section>
       </div>
     </main>
