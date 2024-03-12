c158ce06cd75947bc61552887f0ad54a88e0ffdd
Yordan <yordanpz@hotmail.com>
2024-03-11 23:18:49 -0300

    Refactor document page component and add DocumentNav component

--- src/app/dashboard/document/page.tsx
+++ src/app/dashboard/document/page.tsx
@@ -1,34 +1,13 @@
-'use client'
-import NewDocumentModal from '@/components/NewDocumentModal'
-import { Button } from '@/components/ui/button'
-import { useState } from 'react'
+import DocumentNav from '@/components/DocumentNav'
 
 export default function page() {
-  const [multiresource, setMultiresource] = useState<boolean | undefined>(
-    undefined,
-  )
-  const [isOpen, setIsOpen] = useState<boolean>(false)
 
-  const handleMultiResource = (boolean: boolean) => {
-    setMultiresource(boolean)
-    setIsOpen(true)
-  }
   return (
     <section>
       <div className="flex justify-between flex-wrap">
         <h2>Aqui estaran todos los documentos de la empresa</h2>
         <div className="flex gap-3 flex-wrap">
-          <Button onClick={() => handleMultiResource(true)}>
-            Documento multirecurso
-          </Button>
-          <Button onClick={() => handleMultiResource(false)}>
-            Documento no multirecurso
-          </Button>
-          <NewDocumentModal
-            setIsOpen={setIsOpen}
-            isOpen={isOpen}
-            multiresource={multiresource}
-          />
+          <DocumentNav />
         </div>
       </div>
     </section>
