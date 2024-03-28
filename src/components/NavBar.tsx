c17e8ad5174cc5a9fd5dee668339d00765ed10c6
Diego Castillo <92400440+Dac1977@users.noreply.github.com>
2024-03-28 18:28:12 -0300

    editar perfil

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,30 @@
 'use client'
+
+import { supabase } from '../../supabase/supabase'
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import { useForm, FormProvider } from 'react-hook-form'
+import { UpdateUserPasswordForm } from './UpdateUserPasswordForm'
+import { UploadImage } from './UploadImage'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+
 import ModalCompany from '@/components/ModalCompany'
 import { ModeToggle } from '@/components/ui/ToogleDarkButton'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
@@ -15,7 +41,8 @@ import { useRouter } from 'next/navigation'
 import { useState } from 'react'
 import { IoMdAddCircleOutline } from 'react-icons/io'
 import { LogOutButton } from './LogOutButton'
-
+import { Button } from './ui/button'
+import { Separator } from '@radix-ui/react-select'
 export default function NavBar() {
   const allCompanies = useLoggedUserStore(state => state.allCompanies)
   const actualCompany = useLoggedUserStore(state => state.actualCompany)
@@ -36,7 +63,25 @@ export default function NavBar() {
     setIsOpen(false)
     router.push('/dashboard')
   }
+  const { control, formState, setValue } = useForm()
+  const updateProfileAvatar = async (imageUrl: string) => {
+    try {
+      // Realiza la actualización en la tabla profile usando Supabase
+      const { data, error } = await supabase
+        .from('profile')
+        .update({ avatar: imageUrl })
+        .eq('id', actualUser[0].id)
+      console.log('user: ', actualUser[0].id)
 
+      if (error) {
+        throw error
+      }
+
+      console.log('URL de la imagen actualizada en la tabla profile:', imageUrl)
+    } catch (error) {
+      console.error('Error al actualizar la URL de la imagen:', error)
+    }
+  }
   return (
     <nav className=" flex flex-shrink items-center justify-between  text-white p-4 mb-2">
       <div className="flex items-center">
@@ -162,6 +207,61 @@ export default function NavBar() {
               </Avatar>
             </PopoverTrigger>
             <PopoverContent className="bg-slate-600 border-0">
+              {/* <Button className="mb-2 block">Editar Perfil</Button> */}
+              <Dialog>
+                <DialogTrigger asChild>
+                  <Button variant="outline" className="mb-2 block">
+                    Edit Profile
+                  </Button>
+                </DialogTrigger>
+                <DialogContent className="sm:max-w-[500px]">
+                  <DialogHeader>
+                    <DialogTitle>Editar perfil</DialogTitle>
+                    <DialogDescription>
+                      Aqui se haran cambios en tu perfil
+                    </DialogDescription>
+                  </DialogHeader>
+                  <div className="grid gap-4 py-4">
+                    <div className="w-[300px] flex  gap-2">
+                      <FormProvider {...useForm()}>
+                        <FormField
+                          control={control}
+                          name="company_logo"
+                          render={({ field }) => (
+                            <FormItem className=" max-w-[600px] flex flex-col justify-center">
+                              <FormControl>
+                                <div className="flex lg:items-center flex-wrap md:flex-nowrap flex-col lg:flex-row gap-8">
+                                  <UploadImage
+                                    companyId={actualCompany?.id as string}
+                                    labelInput="Avatar"
+                                    imageBucket="avatar"
+                                    desciption="Sube tu avatar"
+                                    style={{ width: '300px' }}
+                                    // onImageChange={(imageUrl: string) =>
+                                    //   setValue('profile', imageUrl)
+                                    // }
+                                    onImageChange={async imageUrl => {
+                                      setValue('profile', imageUrl)
+                                      await updateProfileAvatar(imageUrl) // Llama a la función para actualizar la URL
+                                    }}
+                                    // onUploadSuccess={onUploadSuccess}
+                                    inputStyle={{ width: '150px' }}
+                                  />
+                                </div>
+                              </FormControl>
+
+                              <FormMessage />
+                            </FormItem>
+                          )}
+                        />
+                      </FormProvider>
+                    </div>
+                    <Separator className="my-4" />
+                    <UpdateUserPasswordForm />
+                  </div>
+                  <DialogFooter></DialogFooter>
+                </DialogContent>
+              </Dialog>
               <LogOutButton />
             </PopoverContent>
           </Popover>
