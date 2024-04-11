3b907a982f1a8e4a569d7eef32b4964f55fa3df1
Yordan <yordanpz@hotmail.com>
2024-04-11 02:46:38 -0300

    Fix bug in login functionality

--- src/app/auditor/[id]/page.tsx
+++ src/app/auditor/[id]/page.tsx
@@ -15,8 +15,13 @@ export default function page({ params }: { params: { id: string } }) {
   const [documents_employees, setDocumentsEmployees] = useState<any[] | null>(
     [],
   )
+  const [resource, setResource] = useState<string | null>(null)
   const [documentUrl, setDocumentUrl] = useState<string | null>(null)
   const fetchDocument = async () => {
+    let document: any[] | null = []
+    let documentType: string | null = null
+    let resourceType: string | null = null
+
     let { data: documents_employee } = await supabase
       .from('documents_employees')
       .select(
@@ -37,18 +42,38 @@ export default function page({ params }: { params: { id: string } }) {
       )
       .eq('id', params.id)
 
-    const document_type_name = documents_employee?.[0]?.document_types?.name
+    if (documents_employee?.length === 0) {
+      let { data: documents_vehicle } = await supabase
+        .from('documents_equipment')
+        .select(
+          `
+      *,
+      document_types(*),
+      applies(*,brand(name),model(name),type_of_vehicle(name), company_id(*,province_id(name)))`,
+        )
+        .eq('id', params.id)
+
+      document = documents_vehicle
+      resourceType = 'documentos-equipos'
+      setResource('vehicle')
+    } else {
+      document = documents_employee
+      resourceType = 'documentos-empleados'
+      setResource('employee')
+    }
+
+    documentType = document?.[0]?.document_types?.name
       .normalize('NFD')
       .replace(/[\u0300-\u036f]/g, '')
       .replace(/\s/g, '')
       .toLowerCase()
       .replace('/', '-')
 
-    const resorceId = documents_employee?.[0]?.applies?.id
+    const resorceId = document?.[0]?.applies?.id
     const { data } = await supabase.storage
       .from('document_files')
-      .list('documentos-empleados', {
-        search: `document-${document_type_name}-${resorceId}`,
+      .list(resourceType, {
+        search: `document-${documentType}-${resorceId}`,
       })
 
     const fileExtension = data?.[0]?.name.split('.').pop()
@@ -56,16 +81,12 @@ export default function page({ params }: { params: { id: string } }) {
     const { data: url } = supabase.storage
       .from('document_files')
       .getPublicUrl(
-        `documentos-empleados/document-${document_type_name}-${resorceId}.${fileExtension}`,
+        `${resourceType}/document-${documentType}-${resorceId}.${fileExtension}`,
       )
 
-    // console.log(url)
-
     setDocumentUrl(url.publicUrl)
-    setDocumentsEmployees(documents_employee)
+    setDocumentsEmployees(document)
   }
-
-  // console.log(documentUrl)
 
   useEffect(() => {
     fetchDocument()
@@ -81,7 +102,7 @@ export default function page({ params }: { params: { id: string } }) {
                 Empresa
               </TabsTrigger>
               <TabsTrigger className="hover:bg-white/30" value="Empleado">
-                Empleado
+                {resource === 'employee' ? 'Empleado' : 'Equipo'}
               </TabsTrigger>
               <TabsTrigger className="hover:bg-white/30" value="Documento">
                 Documento
@@ -135,7 +156,7 @@ export default function page({ params }: { params: { id: string } }) {
                       <span className="font-bold">Provincia:</span>{' '}
                       {
                         documents_employees?.[0]?.applies?.company_id
-                          ?.province_id.name
+                          ?.province_id?.name
                       }
                     </CardDescription>
                     <CardDescription>
@@ -154,14 +175,22 @@ export default function page({ params }: { params: { id: string } }) {
                     </CardDescription>
                     <CardDescription>
                       <span className="font-bold">Fecha de alta:</span>{' '}
-                      {documents_employees?.[0]?.applies?.date_of_admission &&
+                      {(documents_employees?.[0]?.applies?.date_of_admission &&
                         formatDate(
                           documents_employees?.[0]?.applies?.date_of_admission,
                           'dd/MM/yyyy',
                           {
                             locale: es,
                           },
-                        )}
+                        )) ||
+                        (documents_employees?.[0]?.applies?.created_at &&
+                          formatDate(
+                            documents_employees?.[0]?.applies?.created_at,
+                            'dd/MM/yyyy',
+                            {
+                              locale: es,
+                            },
+                          ))}
                     </CardDescription>
                     {documents_employees?.[0]?.applies?.company_id
                       ?.description && (
@@ -182,84 +211,178 @@ export default function page({ params }: { params: { id: string } }) {
                 <div className="p-3">
                   <div className="space-y-3">
                     <CardDescription>
-                      Datos del empleado al que se le solicita el documento
+                      Datos del{' '}
+                      {resource === 'employee' ? 'empleado' : 'equipo'} al que
+                      se le solicita el documento
                     </CardDescription>
                     <div className="flex items-center gap-3">
                       <Avatar className="size-24">
                         <AvatarImage
                           src={documents_employees?.[0]?.applies?.picture}
                           className="rounded-full object-cover"
-                          alt="Logo de la empresa"
+                          alt="Imagen del recurso"
                         />
-                        <AvatarFallback>LOGO</AvatarFallback>
+                        <AvatarFallback>recurso</AvatarFallback>
                       </Avatar>
                       <CardTitle className="font-bold">
-                        {documents_employees?.[0]?.applies.firstname +
-                          ' ' +
-                          documents_employees?.[0]?.applies.lastname}
+                        {resource === 'employee'
+                          ? documents_employees?.[0]?.applies.firstname +
+                            ' ' +
+                            documents_employees?.[0]?.applies.lastname
+                          : documents_employees?.[0]?.applies.domain ||
+                            documents_employees?.[0]?.applies.intern_number}
                       </CardTitle>
                     </div>
                     <div className="space-y-3">
                       <CardDescription>
-                        <span className="font-bold">CUIT:</span>{' '}
-                        {documents_employees?.[0]?.applies?.cuil?.replace(
-                          /(\d{2})(\d{8})(\d{1})/,
-                          '$1-$2-$3',
+                        {resource === 'employee' ? (
+                          <>
+                            <span className="font-bold">CUIT:</span>{' '}
+                            {documents_employees?.[0]?.applies?.cuil?.replace(
+                              /(\d{2})(\d{8})(\d{1})/,
+                              '$1-$2-$3',
+                            )}
+                          </>
+                        ) : (
+                          <>
+                            <span className="font-bold">Numero interno:</span>{' '}
+                            {documents_employees?.[0]?.applies?.intern_number}
+                          </>
                         )}
                       </CardDescription>
                       <CardDescription className="capitalize">
-                        <span className="font-bold">Dirección:</span>{' '}
-                        {documents_employees?.[0]?.applies?.street +
-                          ' ' +
-                          documents_employees?.[0]?.applies?.street_number +
-                          ', ' +
-                          documents_employees?.[0]?.applies?.city.name}
+                        {resource === 'employee' ? (
+                          <>
+                            <span className="font-bold">Dirección:</span>{' '}
+                            {documents_employees?.[0]?.applies?.street +
+                              ' ' +
+                              documents_employees?.[0]?.applies?.street_number +
+                              ', ' +
+                              documents_employees?.[0]?.applies?.city.name}
+                          </>
+                        ) : (
+                          <>
+                            <span className="font-bold">Dominio:</span>{' '}
+                            {documents_employees?.[0]?.applies?.domain}
+                          </>
+                        )}
                       </CardDescription>
                       <CardDescription className="capitalize">
-                        <span className="font-bold">Provincia:</span>{' '}
-                        {documents_employees?.[0]?.applies?.province.name}
+                        {resource === 'employee' ? (
+                          <>
+                            <span className="font-bold">Provincia:</span>{' '}
+                            {documents_employees?.[0]?.applies?.province.name}
+                          </>
+                        ) : (
+                          <>
+                            <span className="font-bold">Marca:</span>{' '}
+                            {documents_employees?.[0]?.applies?.brand?.name}
+                          </>
+                        )}
                       </CardDescription>
                       <CardDescription className="capitalize">
-                        <span className="font-bold">Naciodalidad:</span>{' '}
-                        {documents_employees?.[0]?.applies?.nationality}
+                        {resource === 'employee' ? (
+                          <>
+                            <span className="font-bold">Provincia:</span>{' '}
+                            {documents_employees?.[0]?.applies?.province?.name}
+                          </>
+                        ) : (
+                          <>
+                            <span className="font-bold">Modelo:</span>{' '}
+                            {documents_employees?.[0]?.applies?.model?.name}
+                          </>
+                        )}
                       </CardDescription>
                       <CardDescription>
-                        <span className="font-bold">Teléfono de contacto:</span>{' '}
-                        {documents_employees?.[0]?.applies?.phone}
+                        {resource === 'employee' ? (
+                          <>
+                            <span className="font-bold">
+                              Teléfono de contacto:
+                            </span>{' '}
+                            {documents_employees?.[0]?.applies?.phone}
+                          </>
+                        ) : (
+                          <>
+                            <span className="font-bold">Fecha de alta:</span>{' '}
+                            {documents_employees?.[0]?.applies?.created_at &&
+                              formatDate(
+                                documents_employees?.[0]?.applies?.created_at,
+                                'dd/MM/yyyy',
+                                {
+                                  locale: es,
+                                },
+                              )}
+                          </>
+                        )}
                       </CardDescription>
                       <CardDescription>
-                        <span className="font-bold">Email de contacto:</span>{' '}
-                        {documents_employees?.[0]?.applies?.email}
+                        {resource === 'employee' ? (
+                          <>
+                            <span className="font-bold">
+                              Email de contacto:
+                            </span>{' '}
+                            {documents_employees?.[0]?.applies?.email}
+                          </>
+                        ) : (
+                          <>
+                            <span className="font-bold">Motor:</span>{' '}
+                            {documents_employees?.[0]?.applies?.engine}
+                          </>
+                        )}
                       </CardDescription>
                       <CardDescription>
-                        <span className="font-bold">Fecha de alta:</span>{' '}
-                        {documents_employees?.[0]?.applies?.date_of_admission &&
-                          formatDate(
-                            documents_employees?.[0]?.applies
-                              ?.date_of_admission,
-                            'dd/MM/yyyy',
-                            {
-                              locale: es,
-                            },
-                          )}
+                        {resource === 'employee' ? (
+                          <>
+                            <span className="font-bold">Fecha de alta:</span>{' '}
+                            {documents_employees?.[0]?.applies?.created_at &&
+                              formatDate(
+                                documents_employees?.[0]?.applies?.created_at,
+                                'dd/MM/yyyy',
+                                {
+                                  locale: es,
+                                },
+                              )}
+                          </>
+                        ) : (
+                          <>
+                            <span className="font-bold">Chasis:</span>{' '}
+                            {documents_employees?.[0]?.applies?.chassis}
+                          </>
+                        )}
                       </CardDescription>
                       <div>
-                        <CardDescription>
-                          <span className="font-bold">Afectaciones:</span>{' '}
-                        </CardDescription>
-                        <ul>
-                          <CardDescription>
-                            {documents_employees?.[0]?.applies?.contractor_employee.map(
-                              (contractor: any) => {
-                                return (
-                                  <li key={contractor.contractors.name}>
-                                    {contractor.contractors.name}
-                                  </li>
-                                )
-                              },
-                            )}
-                          </CardDescription>
-                        </ul>
+                        {resource === 'employee' ? (
+                          <>
+                            <CardDescription>
+                              <span className="font-bold">Afectaciones:</span>{' '}
+                            </CardDescription>
+                            <ul>
+                              <CardDescription>
+                                {documents_employees?.[0]?.applies?.contractor_employee?.map(
+                                  (contractor: any) => {
+                                    return (
+                                      <li key={contractor?.contractors?.name}>
+                                        {contractor?.contractors?.name}
+                                      </li>
+                                    )
+                                  },
+                                )}
+                              </CardDescription>
+                            </ul>
+                          </>
+                        ) : (
+                          <>
+                            <CardDescription>
+                              <span className="font-bold">
+                                Tipo de vehiculo:
+                              </span>{' '}
+                              {
+                                documents_employees?.[0]?.applies
+                                  ?.type_of_vehicle?.name
+                              }
+                            </CardDescription>
+                          </>
+                        )}
                       </div>
                     </div>
                   </div>
@@ -269,10 +392,10 @@ export default function page({ params }: { params: { id: string } }) {
             <TabsContent value="Documento" className="space-y-3">
               <Card>
                 <div className="p-3">
-                  <CardDescription>
+                  <CardTitle className="pb-3">
                     Datos del documento que se le solicita al empleado
-                  </CardDescription>
-                  <CardTitle>
+                  </CardTitle>
+                  <CardTitle className="pb-3">
                     {documents_employees?.[0]?.document_types?.name}
                   </CardTitle>
                   <CardDescription>
@@ -317,7 +440,6 @@ export default function page({ params }: { params: { id: string } }) {
                       {documents_employees?.[0]?.document_types?.description}
                     </CardDescription>
                   )}
-                  <h2></h2>
                 </div>
               </Card>
             </TabsContent>
@@ -329,8 +451,8 @@ export default function page({ params }: { params: { id: string } }) {
                     empleado
                   </CardDescription>
                   <div className="w-full flex justify-evenly">
-                    <ApproveDocModal id={params.id} />
-                    <DenyDocModal id={params.id} />
+                    <ApproveDocModal id={params.id} resource={resource} />
+                    <DenyDocModal id={params.id} resource={resource} />
                   </div>
                 </div>
               </Card>
@@ -340,10 +462,10 @@ export default function page({ params }: { params: { id: string } }) {
         <div className="max-w-[70vw]  col-span-2 p-7">
           {documentUrl ? (
             <Card>
-              <CardDescription className="p-3">
+              <CardDescription className="p-3 flex justify-center">
                 <embed
                   src={`${documentUrl}#toolbar=0&navpanes=0&scrollbar=0`}
-                  className="w-full h-screen rounded-xl"
+                  className="max-w-full max-h-screen rounded-xl"
                 />
               </CardDescription>
             </Card>
