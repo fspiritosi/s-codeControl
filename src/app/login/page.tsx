c240d162af4e879cce0cee2f032be0ff207a5416
Yordan <yordanpz@hotmail.com>
2024-04-11 00:29:06 -0300

    Refactor login page to use Card component

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,13 +1,20 @@
 import { CompanyLogoBackground } from '@/components/CompanyLogoBackground'
 import { LoginForm } from '@/components/LoginForm'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 
 export default function Login() {
   return (
     <section className="flex items-center h-screen bg-black/5 justify-between flex-col md:flex-row bg-white dark:bg-neutral-950">
       <CompanyLogoBackground />
       <section className=" md:w-1/2 w-[80%] p-0 md:p-12 lg:p-24 flex flex-col justify-center rounded-3xl">
-        <h2 className="text-2xl pb-5">¡Es un placer verte de nuevo!</h2>
-        <LoginForm />
+        <Card>
+          <CardHeader>
+            <CardTitle>¡Es un placer verte de nuevo!</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <LoginForm />
+          </CardContent>
+        </Card>
       </section>
     </section>
   )
