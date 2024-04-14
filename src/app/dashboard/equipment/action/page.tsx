fe911e0c20ba048ed787bb4934c2b1008ab6ca68
Yordan <yordanpz@hotmail.com>
2024-04-14 20:18:32 -0300

    Corregir estilos en página de acción de empleado y agregar soporte para ocultar la barra lateral en dispositivos móviles

--- src/app/dashboard/equipment/action/page.tsx
+++ src/app/dashboard/equipment/action/page.tsx
@@ -1,4 +1,8 @@
+'use client'
 import { DocumentationDrawer } from '@/components/DocumentationDrawer'
+import { Card } from '@/components/ui/card'
+import { cn } from '@/lib/utils'
+import { useSidebarOpen } from '@/store/sidebar'
 import VehiclesForm from '../../../../components/VehiclesForm'
 
 export default function EmployeeFormAction({
@@ -6,10 +10,22 @@ export default function EmployeeFormAction({
 }: {
   searchParams: any
 }) {
+  const { expanded } = useSidebarOpen()
   return (
-    <section className="flex w-full justify-between">
-      <VehiclesForm />
-      {searchParams.action === 'new' ? false : <DocumentationDrawer />}
-    </section>
+    <Card
+      className={cn(
+        'md:mx-7 py-4 px-6',
+        expanded ? 'md:max-w-[calc(100vw-198px)]' : 'md:max-w-[calc(100vw)]',
+      )}
+    >
+      <section className="grid grid-cols-1 xl:grid-cols-5">
+        <div className=" col-span-4">
+          <VehiclesForm />
+        </div>
+        <div className="xl:max-w-[40vw]  col-span-1 flex justify-center w-full">
+          {searchParams.action === 'new' ? false : <DocumentationDrawer />}
+        </div>
+      </section>
+    </Card>
   )
 }
