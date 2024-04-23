daa6d9f977773d4df261f4618edfed052bec266f
Yordan <yordanpz@hotmail.com>
2024-04-22 23:20:38 -0300

    Agregar componente EPendingDocumentTable a la carpeta componentDashboard en EPendingDocumentTable.tsx

---
+++ src/app/dashboard/componentDashboard/EPendingDocumentTable.tsx
@@ -0,0 +1,19 @@
+'use client'
+import { useLoggedUserStore } from '@/store/loggedUser'
+import { ExpiredColums } from '../colums'
+import { ExpiredDataTable } from '../data-table'
+
+function EPendingDocumentTable() {
+  const employees = useLoggedUserStore(
+    state => state.pendingDocuments,
+  )?.employees
+  return (
+    <ExpiredDataTable
+      data={employees || []}
+      columns={ExpiredColums}
+      pending={true}
+    />
+  ) 
+}
+
+export default EPendingDocumentTable
