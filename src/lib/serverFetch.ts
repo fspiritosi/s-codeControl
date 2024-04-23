1413169658473a5b344c164f1d9d4cd160ac1573
Yordan <yordanpz@hotmail.com>
2024-04-22 23:21:13 -0300

    Actualizar funciones de obtención de documentos y empleados

---
+++ src/lib/serverFetch.ts
@@ -0,0 +1,81 @@
+'use server'
+import { cookies } from 'next/headers'
+import { supabase } from '../../supabase/supabase'
+
+const actualCompany = cookies().get('actualCompanyId')?.value
+
+export async function getDocumentsEmployees() {
+  let { data, error } = await supabase
+    .from('documents_employees')
+    .select(
+      `*,
+  employees:employees(*,contractor_employee(
+    contractors(
+      *
+    )
+  )),
+  document_types:document_types(*)
+`,
+    )
+    .not('employees', 'is', null)
+    .eq('employees.company_id', actualCompany)
+
+  return data
+}
+
+export async function getDocumentsEquipment() {
+  let { data, error } = await supabase
+    .from('documents_equipment')
+    .select(
+      `
+    *,
+    document_types:document_types(*),
+    applies(*,type(*),type_of_vehicle(*),model(*),brand(*))
+    `,
+    )
+    .eq('applies.company_id', actualCompany)
+    .not('applies', 'is', null)
+
+  return data
+}
+
+export async function getEmployees() {
+  let { data, error } = await supabase
+    .from('employees')
+    .select(
+      `*, city (
+        name
+      ),
+      province(
+        name
+      ),
+      workflow_diagram(
+        name
+      ),
+      hierarchical_position(
+        name
+      ),
+      birthplace(
+        name
+      ),
+      contractor_employee(
+        contractors(
+          *
+        )
+      )`,
+    )
+    .eq('company_id', actualCompany)
+    .eq('is_active', true)
+
+  return data
+}
+
+export async function getEquipment() {
+  const { data, error: error2 } = await supabase
+  .from('vehicles')
+  .select('*')
+  .eq('company_id', actualCompany)
+  .eq('is_active', true)
+
+  return data
+}
