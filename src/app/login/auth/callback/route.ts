91c76d21d3e9ea1e9d57463d43dd67dc6b587da8
Yordan <yordanpz@hotmail.com>
2024-05-20 20:10:12 -0300

    Url fixing

--- src/app/login/auth/callback/route.ts
+++ src/app/login/auth/callback/route.ts
@@ -69,44 +69,6 @@ export async function GET(request: Request) {
   const next = searchParams.get('next') ?? '/dashboard'
   const cookieStore = cookies()
 
-  const verified = searchParams.get('verified')
-  console.log(verified, 'verified')
-
-  if (verified) {
-    const supabase = createServerClient(
-      process.env.NEXT_PUBLIC_SUPABASE_URL!,
-      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-      {
-        cookies: {
-          get(name: string) {
-            return cookieStore.get(name)?.value
-          },
-          set(name: string, value: string, options: CookieOptions) {
-            cookieStore.set({ name, value, ...options })
-          },
-          remove(name: string, options: CookieOptions) {
-            cookieStore.delete({ name, ...options })
-          },
-        },
-      },
-    )
-
-    // const supabase = supabaseBrowser()
-
-    const {
-      data: { session },
-    } = await supabase.auth.getSession()
-
-    console.log(session, 'session')
-
-    const { error, data } = await supabase.auth.setSession({
-      access_token: session?.access_token!,
-      refresh_token: session?.refresh_token!,
-    })
-
-    return NextResponse.redirect(`${origin}${next}`)
-  }
-
   if (code) {
     console.log('code')
     const supabase = createServerClient(
