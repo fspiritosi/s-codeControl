b5ae7b0111b86dc68663d12fe97b31a181552490
Yordan <yordanpz@hotmail.com>
2024-06-07 13:32:01 -0300

    feat: Improve DisplayCreatedForms component and add form selection functionality

--- src/app/dashboard/maintenance/new/page.tsx
+++ src/app/dashboard/maintenance/new/page.tsx
@@ -1,20 +1,11 @@
 import { Card } from '@/components/ui/card'
-import { supabaseServer } from '@/lib/supabase/server'
-import { cookies } from 'next/headers'
 import { FormCustomContainer } from './components/FormCustomContainer'
 
-export default async function MailPage() {
-  const supabase = supabaseServer()
-  const cookiesStore = cookies()
-  const companyId = cookiesStore.get('actualComp')
-  const { data, error } = await supabase
-    .from('custom_form')
-    .select('*')
-    .eq('company_id', companyId?.value)
+export default function MailPage() {
   return (
     <div className="hidden flex-col md:flex mt-6 md:mx-7 overflow-hidden max-h-full">
       <Card className="p-0">
-        <FormCustomContainer createdForms={data} />
+        <FormCustomContainer />
       </Card>
     </div>
   )
