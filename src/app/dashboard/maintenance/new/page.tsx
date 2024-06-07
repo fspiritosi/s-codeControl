1b32fe9210755375652646ae63dc7cea2305cf97
Yordan <yordanpz@hotmail.com>
2024-06-06 23:06:30 -0300

    Agregar lógica para obtener formularios personalizados desde la base de datos

--- src/app/dashboard/maintenance/new/page.tsx
+++ src/app/dashboard/maintenance/new/page.tsx
@@ -1,11 +1,23 @@
 import { Card } from '@/components/ui/card'
+import { supabaseServer } from '@/lib/supabase/server'
+import { cookies } from 'next/headers'
 import { FormCustomContainer } from './components/FormCustomContainer'
 
-export default function MailPage() {
+export default async function MailPage() {
+  const supabase = supabaseServer()
+  const cookiesStore = cookies()
+  const companyId = cookiesStore.get('actualComp')
+  const { data, error } = await supabase
+    .from('custom_form')
+    .select('*')
+    .eq('company_id', companyId?.value)
+  console.log(data)
+
+  console.log(error)
   return (
     <div className="hidden flex-col md:flex mt-6 md:mx-7 overflow-hidden max-h-full">
       <Card className="p-0">
-        <FormCustomContainer />
+        <FormCustomContainer createdForms={data} />
       </Card>
     </div>
   )
