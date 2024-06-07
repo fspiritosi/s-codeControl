02196ac44e7a5453805d416775486e263e5d6b4c
Yordan <yordanpz@hotmail.com>
2024-06-07 13:04:55 -0300

    feat: Improve DisplayCreatedForms component and add form selection functionality

--- src/app/dashboard/maintenance/new/components/DisplayCreatedForms.tsx
+++ src/app/dashboard/maintenance/new/components/DisplayCreatedForms.tsx
@@ -1,23 +1,40 @@
+'use client'
+import { Button } from '@/components/ui/button'
 import { Card, CardTitle } from '@/components/ui/card'
-import { ScrollArea } from '@/components/ui/scroll-area'
+import { Campo } from '@/types/types'
+import { ReaderIcon } from '@radix-ui/react-icons'
+import { Dispatch, SetStateAction } from 'react'
 
 export default function DisplayCreatedForms({
   createdForms,
+  setSelectedForm
 }: {
   createdForms: any[] | null
+  setSelectedForm: Dispatch<SetStateAction<Campo[] | undefined>>
 }) {
-  console.log(createdForms)
+  const handleSelectForm = (index: number) => {
+    setSelectedForm(createdForms?.[index].form as Campo[])
+  }
 
   return (
-    <ScrollArea className="flex flex-col gap-2 p-4 pt-0 space-y-2  max-h-[68vh]">
-      <CardTitle>Forms creados</CardTitle>
-      {createdForms?.map((form, index) => {
-        return (
-          <Card className='p-2' key={index}>
-            <p>{form.name}</p>
-          </Card>
-        )
-      })}
-    </ScrollArea>
+    <section className="p-4 pt-0 max-h-[68vh]">
+      <CardTitle className="mb-2 text-xl">Forms creados</CardTitle>
+
+      <div className="space-y-3">
+        {createdForms?.map((form, index) => {
+          return (
+            <Card className="p-2 flex items-center justify-between" key={index}>
+              <CardTitle className="capitalize flex items-center">
+                <ReaderIcon className="size-6 mr-2" />
+                {form.name}
+              </CardTitle>
+              <Button onClick={() => handleSelectForm(index)}>
+                Vista previa
+              </Button>
+            </Card>
+          )
+        })}
+      </div>
+    </section>
   )
 }
