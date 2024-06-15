6844115724bcc5792fc01ea573475fc27e8bdb26
Yordan <yordanpz@hotmail.com>
2024-06-14 21:30:46 -0300

    Nuevos estilos en el template

--- src/components/EmailTemplate.tsx
+++ src/components/EmailTemplate.tsx
@@ -19,251 +19,240 @@ export const EmailTemplate: React.FC<Readonly<EmailTemplateProps>> = ({
   reason,
   emailInfo,
 }) => (
-  <div>
-    <html lang="es">
-      <Head>
-        <meta content="text/html; charset=UTF-8" />
-      </Head>
+  <html lang="es">
+    <Head>
+      <meta content="text/html; charset=UTF-8" />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+    </Head>
 
-      <body
-        style={{
-          backgroundColor: '#f3f3f5',
-          fontFamily: 'HelveticaNeue, Helvetica, Arial, sans-serif',
-        }}
+    <body
+      style={{ backgroundColor: '#f3f3f5', fontFamily: 'Poppins, sans-serif' }}
+    >
+      <table
+        align="center"
+        width="100%"
+        border={0}
+        cellPadding="0"
+        cellSpacing="0"
+        style={{ maxWidth: '680px', margin: '0 auto' }}
       >
-        <table
-          align="center"
-          width="100%"
-          border={0}
-          cellPadding="0"
-          cellSpacing="0"
-          style={{
-            maxWidth: '100%',
-            width: '680px',
-            margin: '0 auto',
-            backgroundColor: '#ffffff',
-          }}
-        >
-          <tbody>
-            <tr style={{ width: '100%' }}>
-              <td>
-                <table
-                  align="center"
-                  width="100%"
-                  border={0}
-                  cellPadding="0"
-                  cellSpacing="0"
-                  style={{
-                    borderRadius: '0px 0px 0 0',
-                    display: 'flex',
-                    flexDirection: 'column',
-                    backgroundColor: '#2b2d6e',
-                  }}
-                >
-                  <tbody>
-                    <tr>
-                      <td>
-                        <table
-                          align="center"
-                          width="100%"
-                          border={0}
-                          cellPadding="0"
-                          cellSpacing="0"
-                        >
-                          <tbody style={{ width: '100%' }}>
-                            <tr style={{ width: '100%' }}>
-                              <td style={{ padding: '20px 30px 15px' }}>
-                                <h1
-                                  style={{
-                                    color: '#fff',
-                                    fontSize: '27px',
-                                    fontWeight: 'bold',
-                                    lineHeight: '27px',
-                                  }}
-                                >
-                                  Codecontrol.com.ar
-                                </h1>
-                                <p
-                                  style={{
-                                    fontSize: '17px',
-                                    lineHeight: '24px',
-                                    margin: '16px 0',
-                                    color: '#fff',
-                                  }}
-                                >
-                                  Documento Rechazado
-                                </p>
-                              </td>
-                              <td style={{ padding: '30px 10px' }}>
-                                <img
-                                  src="https://zktcbhhlcksopklpnubj.supabase.co/storage/v1/object/public/logo/Logo%20Juegos%20gaming%20moderno%20azul%20y%20violeta%20(4).png"
-                                  style={{
-                                    display: 'block',
-                                    outline: 'none',
-                                    border: 'none',
-                                    textDecoration: 'none',
-                                    maxWidth: '100%',
-                                  }}
-                                  width="140"
-                                />
-                              </td>
-                            </tr>
-                          </tbody>
-                        </table>
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>
-                <table
-                  align="center"
-                  width="100%"
-                  border={0}
-                  cellPadding="0"
-                  cellSpacing="0"
-                  style={{ padding: '30px 30px 40px 30px' }}
-                >
-                  <tbody>
-                    <tr>
-                      <td>
-                        <h2
-                          style={{
-                            margin: '0 0 15px',
-                            fontWeight: 'bold',
-                            fontSize: '21px',
-                            lineHeight: '21px',
-                            color: '#0c0d0e',
-                          }}
-                        >
-                          Hola! nos comunicamos de Codecontrol para avisarte que
-                          tienes un documento rechazado, en la bandeja de
-                          notificaciones podras verlo y hacer una actualización
-                          del mismo
-                        </h2>
-
-                        <hr
-                          style={{
-                            width: '100%',
-                            border: 'none',
-                            borderTop: '1px solid #eaeaea',
-                            margin: '30px 0',
-                          }}
-                        />
-
-                        <ul>
-                          <p
-                            style={{
-                              fontSize: '15px',
-                              lineHeight: '21px',
-                              margin: '16px 0',
-                              color: '#3c3f44',
-                            }}
-                          >
-                            Compañia: {emailInfo.company_name}.
-                          </p>
-                          <p
-                            style={{
-                              fontSize: '15px',
-                              lineHeight: '21px',
-                              margin: '16px 0',
-                              color: '#3c3f44',
-                            }}
-                          >
-                            Recurso: {emailInfo.resource_name}.
-                          </p>
-                          <p
-                            style={{
-                              fontSize: '15px',
-                              lineHeight: '21px',
-                              margin: '16px 0',
-                              color: '#3c3f44',
-                            }}
-                          >
-                            Documento: {emailInfo.document_name}.
-                          </p>
-                          {emailInfo.document_number && (
-                            <p
+        <tbody>
+          <tr>
+            <td>
+              <table
+                width="100%"
+                border={0}
+                cellPadding="0"
+                cellSpacing="0"
+                style={{
+                  backgroundColor: '#f8fafc',
+                  borderRadius: '0.5rem 0.5rem 0 0',
+                }}
+              >
+                <tbody>
+                  <tr>
+                    <td style={{ padding: '30px' }}>
+                      <h1
+                        style={{
+                          color: '#2b2d6e',
+                          fontSize: '32px',
+                          fontWeight: '700',
+                          lineHeight: '36px',
+                          marginBottom: '16px',
+                        }}
+                      >
+                        Codecontrol
+                      </h1>
+                      <p
+                        style={{
+                          fontSize: '20px',
+                          lineHeight: '28px',
+                          color: '#6b7280',
+                        }}
+                      >
+                        Notificación de Documento
+                      </p>
+                    </td>
+                    <td style={{ padding: '30px' }}>
+                      <img
+                        src="https://zktcbhhlcksopklpnubj.supabase.co/storage/v1/object/public/logo/24417298440.png"
+                        style={{ display: 'block', maxWidth: '100%' }}
+                        width="140"
+                        alt="logo codecontrol"
+                      />
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+              <table
+                width="100%"
+                border={0}
+                cellPadding="0"
+                cellSpacing="0"
+                style={{
+                  backgroundColor: '#fff',
+                  padding: '40px',
+                  borderRadius: '0 0 0.5rem 0.5rem',
+                }}
+              >
+                <tbody>
+                  <tr>
+                    <td>
+                      <h2
+                        style={{
+                          margin: '0 0 20px',
+                          fontWeight: '700',
+                          fontSize: '24px',
+                          lineHeight: '28px',
+                          color: '#2b2d6e',
+                        }}
+                      >
+                        ¡Importante! Tenemos una actualización sobre tu
+                        documentación.
+                      </h2>
+                      <p
+                        style={{
+                          fontSize: '16px',
+                          lineHeight: '24px',
+                          color: '#6b7280',
+                          marginBottom: '30px',
+                        }}
+                      >
+                        Hemos detectado que uno de tus documentos ha sido
+                        rechazado y requiere tu atención inmediata. Por favor,
+                        revisa los detalles a continuación y realiza las
+                        acciones necesarias en tu bandeja de notificaciones.
+                      </p>
+                      <table
+                        width="100%"
+                        border={0}
+                        cellPadding="0"
+                        cellSpacing="0"
+                        style={{
+                          borderCollapse: 'separate',
+                          borderSpacing: '10px',
+                        }}
+                      >
+                        <tbody>
+                          <tr>
+                            <td
                               style={{
-                                fontSize: '15px',
-                                lineHeight: '21px',
-                                margin: '16px 0',
-                                color: '#3c3f44',
+                                backgroundColor: '#f8fafc',
+                                padding: '30px',
+                                borderRadius: '0.5rem',
+                                border: '1px solid #e0e0e0',
                               }}
                             >
-                              Numero de documento: {emailInfo.document_number}.
-                            </p>
-                          )}
-                          <p
-                            style={{
-                              fontSize: '15px',
-                              lineHeight: '21px',
-                              margin: '16px 0',
-                              color: '#3c3f44',
-                            }}
-                          >
-                            Motivo: {reason}.
-                          </p>
-                        </ul>
-
-                        <hr
-                          style={{
-                            width: '100%',
-                            border: 'none',
-                            borderTop: '1px solid #eaeaea',
-                            margin: '30px 0',
-                          }}
-                        />
-                        <h2
-                          style={{
-                            margin: '0 0 15px',
-                            fontWeight: 'bold',
-                            fontSize: '21px',
-                            lineHeight: '21px',
-                            color: '#0c0d0e',
-                          }}
-                        >
-                          Para ver los documentos diríjase a la app
-                        </h2>
-                        <table
-                          align="center"
-                          width="100%"
-                          border={0}
-                          cellPadding="0"
-                          cellSpacing="0"
-                          style={{ marginTop: '24px', display: 'block' }}
-                        >
-                          <tbody>
-                            <tr>
-                              <td>
-                                <a
-                                  href="https://codecontrol.com.ar"
+                              <h3
+                                style={{
+                                  margin: '0 0 20px',
+                                  fontWeight: '700',
+                                  fontSize: '18px',
+                                  lineHeight: '24px',
+                                  color: '#2b2d6e',
+                                }}
+                              >
+                                Detalles del Documento Rechazado
+                              </h3>
+                              <p
+                                style={{
+                                  fontSize: '16px',
+                                  lineHeight: '24px',
+                                  color: '#6b7280',
+                                  marginBottom: '10px',
+                                }}
+                              >
+                                <strong>Compañía:</strong>{' '}
+                                {emailInfo.company_name}
+                              </p>
+                              <p
+                                style={{
+                                  fontSize: '16px',
+                                  lineHeight: '24px',
+                                  color: '#6b7280',
+                                  marginBottom: '10px',
+                                }}
+                              >
+                                <strong>Recurso:</strong>{' '}
+                                {emailInfo.resource_name}
+                              </p>
+                              <p
+                                style={{
+                                  fontSize: '16px',
+                                  lineHeight: '24px',
+                                  color: '#6b7280',
+                                  marginBottom: '10px',
+                                }}
+                              >
+                                <strong>Documento:</strong>{' '}
+                                {emailInfo.document_name}
+                              </p>
+                              {emailInfo.document_number && (
+                                <p
                                   style={{
-                                    color: '#fff',
-                                    textDecoration: 'none',
-                                    backgroundColor: '#0095ff',
-                                    border: '1px solid #0077cc',
-                                    fontSize: '17px',
-                                    lineHeight: '17px',
-                                    padding: '13px 17px',
-                                    borderRadius: '4px',
-                                    maxWidth: '120px',
+                                    fontSize: '16px',
+                                    lineHeight: '24px',
+                                    color: '#6b7280',
+                                    marginBottom: '10px',
                                   }}
-                                  target="_blank"
                                 >
-                                  ir a codecontrol.com.ar
-                                </a>
-                              </td>
-                            </tr>
-                          </tbody>
-                        </table>
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </body>
-    </html>
-  </div>
+                                  <strong>Número de Documento:</strong>{' '}
+                                  {emailInfo.document_number}
+                                </p>
+                              )}
+                              <p
+                                style={{
+                                  fontSize: '16px',
+                                  lineHeight: '24px',
+                                  color: '#6b7280',
+                                }}
+                              >
+                                <strong>Motivo:</strong> {reason}
+                              </p>
+                            </td>
+                          </tr>
+                        </tbody>
+                      </table>
+                      <table
+                        align="center"
+                        width="100%"
+                        border={0}
+                        cellPadding="0"
+                        cellSpacing="0"
+                        style={{ marginTop: '30px' }}
+                      >
+                        <tbody>
+                          <tr>
+                            <td>
+                              <a
+                                href="https://codecontrol.com.ar/dashboard"
+                                style={{
+                                  color: '#fff',
+                                  textDecoration: 'none',
+                                  backgroundColor: '#2b2d6e',
+                                  fontSize: '16px',
+                                  lineHeight: '20px',
+                                  padding: '12px 20px',
+                                  borderRadius: '0.5rem',
+                                  display: 'inline-block',
+                                }}
+                                target="_blank"
+                              >
+                                Ir a Codecontrol
+                              </a>
+                            </td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </body>
+  </html>
 )
