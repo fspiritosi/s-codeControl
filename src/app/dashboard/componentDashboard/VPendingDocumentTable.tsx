5b5d5d08b3b28b3978e5ae4fbceaa8ba6f411151
Yordan <yordanpz@hotmail.com>
2024-06-19 18:11:23 -0300

    Add localStorageName prop to VPendingDocumentTable component

--- src/app/dashboard/componentDashboard/VPendingDocumentTable.tsx
+++ src/app/dashboard/componentDashboard/VPendingDocumentTable.tsx
@@ -4,16 +4,15 @@ import { ExpiredColums } from '../colums'
 import { ExpiredDataTable } from '../data-table'
 
 function VPendingDocumentTable() {
-  const vehicles = useLoggedUserStore(
-    state => state.pendingDocuments,
-  )?.vehicles
+  const vehicles = useLoggedUserStore(state => state.pendingDocuments)?.vehicles
   return (
     <ExpiredDataTable
       data={vehicles || []}
       columns={ExpiredColums}
       pending={true}
+      localStorageName="dashboardVPendingColumns"
     />
-  ) 
+  )
 }
 
 export default VPendingDocumentTable
