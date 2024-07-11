d113ba33ab5df90ca99daeaa8a599d08c7e8b613
Fabricio Spiritosi <fspiritosi@gmail.com>
2024-07-10 23:31:48 -0300

    modificando Admin

--- src/app/admin/components/createUser.tsx
+++ src/app/admin/components/createUser.tsx
@@ -1,7 +1,21 @@
+'use client'
 import { Button } from '@/components/ui/button';
 import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import {
+  AlertDialog,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog';
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+
+import { useState } from 'react';
 
 export default function CreateUser() {
+  const [open, setOpen] = useState(false);
   return (
     <>
       <Card className="sm:col-span-2" x-chunk="dashboard-05-chunk-0">
@@ -12,7 +26,17 @@ export default function CreateUser() {
           </CardDescription>
         </CardHeader>
         <CardFooter>
-          <Button>Create New Order</Button>
+          <AlertDialog open={open} onOpenChange={() => setOpen(!open)}>
+            <AlertDialogTrigger>
+              <Button>Crear Usuario</Button>
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+              <AlertDialogTitle>Crear nuevo usuario</AlertDialogTitle>
+              <AlertDialogDescription>
+                Aca va el formulario, ver de pasarlo a un componente
+              </AlertDialogDescription>
+            </AlertDialogContent>
+          </AlertDialog>
         </CardFooter>
       </Card>
     </>
