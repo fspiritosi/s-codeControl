785f952c414ccfde3472d64f9771313672d64d41
Diego Castillo <diegodac77@gmail.com>
2024-07-21 22:28:08 -0300

    convenios7

--- src/app/dashboard/company/actualCompany/covenant/columnsCategory.tsx
+++ src/app/dashboard/company/actualCompany/covenant/columnsCategory.tsx
@@ -52,6 +52,11 @@ import { useForm } from 'react-hook-form';
 import { toast } from 'sonner';
 import { z } from 'zod';
 import { supabase } from '../../../../../../supabase/supabase';
+
+const editCategorySchema = z.object({
+  category: z.string().nonempty('El nombre de la categoría es requerido.'),
+}); 
+
 const formSchema = z.object({
   
   termination_date: z.date({
@@ -78,15 +83,21 @@ export const columnsCategory: ColumnDef<Colum>[] = [
       }
 
       const [showModal, setShowModal] = useState(false);
+      const [showCategoryModal, setShowCategoryModal] = useState(false);
       const [integerModal, setIntegerModal] = useState(false);
       const [id, setId] = useState('');
-      const [showInactive, setShowInactive] = useState('');
-      const [showDeletedContact, setShowDeletedContact] = useState(false);
+     
       const category = row.original;
-
+      console.log(category)
       const handleOpenModal = (id: string) => {
         setId(id);
         setShowModal(!showModal);
+      };
+
+      const handleOpenCategoryModal = (id: string) => {
+        setId(id);
+        
+        setShowCategoryModal(!showCategoryModal);
       };
       const actualCompany = useLoggedUserStore((state) => state.actualCompany);
 
@@ -98,6 +109,12 @@ export const columnsCategory: ColumnDef<Colum>[] = [
 
       const { errorTranslate } = useEdgeFunctions();
 
+      const formEditCategory = useForm<z.infer<typeof editCategorySchema>>({
+        resolver: zodResolver(editCategorySchema),
+        defaultValues: {
+          category: '', 
+        },
+      });
       const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
@@ -121,9 +138,7 @@ export const columnsCategory: ColumnDef<Colum>[] = [
               .select();
 
             setIntegerModal(!integerModal);
-            //setInactive(data as any)
-            setShowDeletedContact(false);
-
+            
             if (error) {
               throw new Error(handleSupabaseError(error.message));
             }
@@ -170,6 +185,34 @@ export const columnsCategory: ColumnDef<Colum>[] = [
           }
         );
       }
+
+      async function editCategory(values: z.infer<typeof editCategorySchema>) {
+        toast.promise(
+          async () => {
+            const supabase = supabaseBrowser();
+
+            const { data, error } = await supabase
+              .from('category')
+              .update({
+                name: values.category, 
+              })
+              .eq('id', category.id)
+              
+              .select();
+
+            setShowCategoryModal(!showCategoryModal);
+
+            if (error) {
+              throw new Error(handleSupabaseError(error.message));
+            }
+          },
+          {
+            loading: 'Editando...',
+            success: `Convenio editado`,
+            error: (error) => error.message,
+          }
+        );
+      }
       const today = new Date();
       const nextMonth = addMonths(new Date(), 1);
       const [month, setMonth] = useState<Date>(nextMonth);
@@ -209,44 +252,7 @@ export const columnsCategory: ColumnDef<Colum>[] = [
                   <div className="w-full">
                     <Form {...form}>
                       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-                        {/* <FormField
-                          control={form.control}
-                          name="reason_for_termination"
-                          render={({ field }) => (
-                            <FormItem>
-                              <FormLabel>Motivo de Baja</FormLabel>
-                              <Input
-                                className="input w-[250px]"
-                                placeholder="Escribe el motivo"
-                                maxLength={80} // Limitar a 80 caracteres
-                                value={field.value}
-                                onChange={(e: any) => {
-                                  field.onChange(e.target.value);
-                                }}
-                              /> */}
-                              {/* <Select
-                                onValueChange={field.onChange}
-                                defaultValue={field.value}
-                              >
-                                <FormControl>
-                                  <SelectTrigger>
-                                    <SelectValue placeholder="Selecciona la razón" />
-                                  </SelectTrigger>
-                                </FormControl>
-                                <SelectContent>
-                                  <SelectItem value="Fin del contrato">Fin del Contrato</SelectItem>
-                                  <SelectItem value="Cerro la empresa">Cerro la Empresa</SelectItem>
-                                  <SelectItem value="Otro">Otro</SelectItem>
-                                </SelectContent>
-                              </Select> */}
-                              {/* <FormDescription>
-                                Elige la razón por la que deseas dar de baja el
-                                contacto
-                              </FormDescription>
-                              <FormMessage /> */}
-                            {/* </FormItem>
-                          )} */}
-                        {/* /> */}
+                        
                         <FormField
                           control={form.control}
                           name="termination_date"
@@ -275,17 +281,7 @@ export const columnsCategory: ColumnDef<Colum>[] = [
                                   </FormControl>
                                 </PopoverTrigger>
                                 <PopoverContent className="w-auto p-0" align="start">
-                                  {/* <Calendar
-                                    mode="single"
-                                    selected={field.value}
-                                    onSelect={field.onChange}
-                                    disabled={date =>
-                                      date > new Date() ||
-                                      date < new Date('1900-01-01')
-                                    }
-                                    initialFocus
-                                    locale={es}
-                                  /> */}
+                                  
                                   <Select
                                     onValueChange={(e) => {
                                       setMonth(new Date(e));
@@ -347,6 +343,44 @@ export const columnsCategory: ColumnDef<Colum>[] = [
               </DialogContent>
             </Dialog>
           )}
+          {showCategoryModal && (
+            <Dialog defaultOpen onOpenChange={() => setShowCategoryModal(!showCategoryModal)}>
+              <DialogContent className="dark:bg-slate-950">
+                <DialogTitle>Editar Sindicato</DialogTitle>
+                <DialogDescription>
+                  ¿Estás seguro de que deseas editar este sindicato?, completa los campos para continuar.
+                </DialogDescription>
+                <DialogFooter>
+                  <div className="w-full">
+                    <Form {...formEditCategory}>
+                      <form onSubmit={formEditCategory.handleSubmit(editCategory)} className="space-y-8">
+                        <FormField
+                          control={formEditCategory.control}
+                          name="category"
+                          render={({ field }) => (
+                            <FormItem>
+                              <FormLabel>Nombre</FormLabel>
+                              <FormControl>
+                                <Input placeholder="Nombre de la categoría" {...field} />
+                              </FormControl>
+                              <FormDescription>Nombre de la categoría</FormDescription>
+                              <FormMessage />
+                            </FormItem>
+                          )}
+                        />
+                        <div className="flex gap-4 justify-end">
+                          <Button variant="primary" type="submit">
+                            Editar
+                          </Button>
+                          <DialogClose>Cancelar</DialogClose>
+                        </div>
+                      </form>
+                    </Form>
+                  </div>
+                </DialogFooter>
+              </DialogContent>
+            </Dialog>
+          )}
           <DropdownMenuTrigger asChild>
             <Button variant="ghost" className="h-8 w-8 p-0">
               <span className="sr-only">Open menu</span>
@@ -356,21 +390,17 @@ export const columnsCategory: ColumnDef<Colum>[] = [
           <DropdownMenuContent align="end">
             <DropdownMenuLabel>Opciones</DropdownMenuLabel>
             <DropdownMenuSeparator />
-            {/* <DropdownMenuItem onClick={() => navigator.clipboard.writeText(covenant.constact_email)}>
-              Copiar email
-            </DropdownMenuItem>
-            <DropdownMenuItem>
-              <Link className="w-full" href={`/dashboard/company/contact/action?action=view&id=${contacts?.name}`}>
-                Ver Contacto
-              </Link>
-            </DropdownMenuItem>
             <DropdownMenuItem>
               {role !== 'Invitado' && (
-                <Link className="w-full" href={`/dashboard/company/contact/action?action=edit&id=${contacts?.id}`}>
-                  Editar Contacto
-                </Link>
+                <Fragment>
+
+                  <Button variant="destructive" onClick={() => handleOpenCategoryModal(category?.id)} className="text-sm">
+                    Editar Categoría
+                  </Button>
+
+                </Fragment>
               )}
-            </DropdownMenuItem> */}
+            </DropdownMenuItem>
             <DropdownMenuItem>
               {role !== 'Invitado' && (
                 <Fragment>
@@ -391,17 +421,7 @@ export const columnsCategory: ColumnDef<Colum>[] = [
       );
     },
   },
-  // {
-  //   accessorKey: 'name',
-  //   header: ({ column }: { column: any }) => {
-  //     return (
-  //       <Button variant="ghost" onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')} className="p-0">
-  //         Nombre
-  //         <ArrowUpDown className="ml-2 h-4 w-4" />
-  //       </Button>
-  //     );
-  //   },
-  // },
+  
   {
     accessorKey: 'name',
     header: 'Categoría',
--- src/app/dashboard/company/actualCompany/covenant/columnsCct.tsx
+++ src/app/dashboard/company/actualCompany/covenant/columnsCct.tsx
@@ -52,6 +52,11 @@ import { useForm } from 'react-hook-form';
 import { toast } from 'sonner';
 import { z } from 'zod';
 import { supabase } from '../../../../../../supabase/supabase';
+
+
+const editCovenantSchema = z.object({
+  covenant: z.string().nonempty('El nombre del convenio es requerido.'),
+}); 
 const formSchema = z.object({
   
   termination_date: z.date({
@@ -78,15 +83,21 @@ export const columns: ColumnDef<Colum>[] = [
       }
 
       const [showModal, setShowModal] = useState(false);
+      const [showCovenantModal, setShowCovenantModal] = useState(false);
       const [integerModal, setIntegerModal] = useState(false);
       const [id, setId] = useState('');
-      const [showInactive, setShowInactive] = useState('');
-      const [showDeletedContact, setShowDeletedContact] = useState(false);
+      
       const covenant = row.original;
 
       const handleOpenModal = (id: string) => {
         setId(id);
         setShowModal(!showModal);
+      };
+
+      const handleOpenCovenantModal = (id: string) => {
+        setId(id);
+        
+        setShowCovenantModal(!showCovenantModal);
       };
       const actualCompany = useLoggedUserStore((state) => state.actualCompany);
 
@@ -116,6 +127,13 @@ export const columns: ColumnDef<Colum>[] = [
 
       const { errorTranslate } = useEdgeFunctions();
 
+      const formEditCovenant = useForm<z.infer<typeof editCovenantSchema>>({
+        resolver: zodResolver(editCovenantSchema),
+        defaultValues: {
+          covenant: '', 
+        },
+      });
+
       const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
@@ -139,8 +157,7 @@ export const columns: ColumnDef<Colum>[] = [
               .select();
 
             setIntegerModal(!integerModal);
-            //setInactive(data as any)
-            setShowDeletedContact(false);
+            
 
             if (error) {
               throw new Error(handleSupabaseError(error.message));
@@ -202,6 +219,34 @@ export const columns: ColumnDef<Colum>[] = [
           }
         );
       }
+
+      async function editCovenant(values: z.infer<typeof editCovenantSchema>) {
+        toast.promise(
+          async () => {
+            const supabase = supabaseBrowser();
+
+            const { data, error } = await supabase
+              .from('covenant')
+              .update({
+                name: values.covenant, 
+              })
+              .eq('id', covenant.id)
+              .eq('company_id', actualCompany?.id)
+              .select();
+
+            setShowCovenantModal(!showCovenantModal);
+
+            if (error) {
+              throw new Error(handleSupabaseError(error.message));
+            }
+          },
+          {
+            loading: 'Editando...',
+            success: `Convenio editado`,
+            error: (error) => error.message,
+          }
+        );
+      }
       const today = new Date();
       const nextMonth = addMonths(new Date(), 1);
       const [month, setMonth] = useState<Date>(nextMonth);
@@ -241,44 +286,7 @@ export const columns: ColumnDef<Colum>[] = [
                   <div className="w-full">
                     <Form {...form}>
                       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-                        {/* <FormField
-                          control={form.control}
-                          name="reason_for_termination"
-                          render={({ field }) => (
-                            <FormItem>
-                              <FormLabel>Motivo de Baja</FormLabel>
-                              <Input
-                                className="input w-[250px]"
-                                placeholder="Escribe el motivo"
-                                maxLength={80} // Limitar a 80 caracteres
-                                value={field.value}
-                                onChange={(e: any) => {
-                                  field.onChange(e.target.value);
-                                }}
-                              /> */}
-                              {/* <Select
-                                onValueChange={field.onChange}
-                                defaultValue={field.value}
-                              >
-                                <FormControl>
-                                  <SelectTrigger>
-                                    <SelectValue placeholder="Selecciona la razón" />
-                                  </SelectTrigger>
-                                </FormControl>
-                                <SelectContent>
-                                  <SelectItem value="Fin del contrato">Fin del Contrato</SelectItem>
-                                  <SelectItem value="Cerro la empresa">Cerro la Empresa</SelectItem>
-                                  <SelectItem value="Otro">Otro</SelectItem>
-                                </SelectContent>
-                              </Select> */}
-                              {/* <FormDescription>
-                                Elige la razón por la que deseas dar de baja el
-                                contacto
-                              </FormDescription>
-                              <FormMessage /> */}
-                            {/* </FormItem>
-                          )} */}
-                        {/* /> */}
+                        
                         <FormField
                           control={form.control}
                           name="termination_date"
@@ -307,17 +315,7 @@ export const columns: ColumnDef<Colum>[] = [
                                   </FormControl>
                                 </PopoverTrigger>
                                 <PopoverContent className="w-auto p-0" align="start">
-                                  {/* <Calendar
-                                    mode="single"
-                                    selected={field.value}
-                                    onSelect={field.onChange}
-                                    disabled={date =>
-                                      date > new Date() ||
-                                      date < new Date('1900-01-01')
-                                    }
-                                    initialFocus
-                                    locale={es}
-                                  /> */}
+                                  
                                   <Select
                                     onValueChange={(e) => {
                                       setMonth(new Date(e));
@@ -379,6 +377,44 @@ export const columns: ColumnDef<Colum>[] = [
               </DialogContent>
             </Dialog>
           )}
+          {showCovenantModal && (
+            <Dialog defaultOpen onOpenChange={() => setShowCovenantModal(!showCovenantModal)}>
+              <DialogContent className="dark:bg-slate-950">
+                <DialogTitle>Editar Sindicato</DialogTitle>
+                <DialogDescription>
+                  ¿Estás seguro de que deseas editar este sindicato?, completa los campos para continuar.
+                </DialogDescription>
+                <DialogFooter>
+                  <div className="w-full">
+                    <Form {...formEditCovenant}>
+                      <form onSubmit={formEditCovenant.handleSubmit(editCovenant)} className="space-y-8">
+                        <FormField
+                          control={formEditCovenant.control}
+                          name="covenant"
+                          render={({ field }) => (
+                            <FormItem>
+                              <FormLabel>Nombre</FormLabel>
+                              <FormControl>
+                                <Input placeholder="Nombre del sindicato" {...field} />
+                              </FormControl>
+                              <FormDescription>Nombre del convenio</FormDescription>
+                              <FormMessage />
+                            </FormItem>
+                          )}
+                        />
+                        <div className="flex gap-4 justify-end">
+                          <Button variant="primary" type="submit">
+                            Editar
+                          </Button>
+                          <DialogClose>Cancelar</DialogClose>
+                        </div>
+                      </form>
+                    </Form>
+                  </div>
+                </DialogFooter>
+              </DialogContent>
+            </Dialog>
+          )}
           <DropdownMenuTrigger asChild>
             <Button variant="ghost" className="h-8 w-8 p-0">
               <span className="sr-only">Open menu</span>
@@ -388,21 +424,18 @@ export const columns: ColumnDef<Colum>[] = [
           <DropdownMenuContent align="end">
             <DropdownMenuLabel>Opciones</DropdownMenuLabel>
             <DropdownMenuSeparator />
-            {/* <DropdownMenuItem onClick={() => navigator.clipboard.writeText(covenant.constact_email)}>
-              Copiar email
-            </DropdownMenuItem>
-            <DropdownMenuItem>
-              <Link className="w-full" href={`/dashboard/company/contact/action?action=view&id=${contacts?.name}`}>
-                Ver Contacto
-              </Link>
-            </DropdownMenuItem>
             <DropdownMenuItem>
               {role !== 'Invitado' && (
-                <Link className="w-full" href={`/dashboard/company/contact/action?action=edit&id=${contacts?.id}`}>
-                  Editar Contacto
-                </Link>
+                <Fragment>
+
+                  <Button variant="destructive" onClick={() => handleOpenCovenantModal(covenant?.id)} className="text-sm">
+                    Editar Convenio
+                  </Button>
+
+                </Fragment>
               )}
-            </DropdownMenuItem> */}
+            </DropdownMenuItem>
+            
             <DropdownMenuItem>
               {role !== 'Invitado' && (
                 <Fragment>
@@ -423,17 +456,7 @@ export const columns: ColumnDef<Colum>[] = [
       );
     },
   },
-  // {
-  //   accessorKey: 'name',
-  //   header: ({ column }: { column: any }) => {
-  //     return (
-  //       <Button variant="ghost" onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')} className="p-0">
-  //         Nombre
-  //         <ArrowUpDown className="ml-2 h-4 w-4" />
-  //       </Button>
-  //     );
-  //   },
-  // },
+  
   {
     accessorKey: 'name',
     header: 'Nombre',
