b09e3714ba3ae8f00a72c70315d912ad28903c59
Fabricio Spiritosi <fspiritosi@gmail.com>
2024-08-04 22:33:22 -0300

    Cambiando el DB, mejorando la vista de carga de novedades

--- src/components/DiagramEmployeeView.tsx
+++ src/components/DiagramEmployeeView.tsx
@@ -18,6 +18,9 @@ type DiamgramParsed = {
 
 function DiagramEmployeeView({diagrams, activeEmployees}:{diagrams:any, activeEmployees:any}) {
 
+console.log(diagrams)
+
+
   function generarDiasEntreFechas({fechaInicio, fechaFin}:{fechaInicio:Date, fechaFin:Date}) {
     const dias = [];
     let fechaActual = new Date(fechaInicio);
@@ -31,53 +34,55 @@ function DiagramEmployeeView({diagrams, activeEmployees}:{diagrams:any, activeEm
   }
   
 
-  function generarFechas(fromDate:string, toDate:string) {
-    let fechas = [];
-    let currentDate = new Date(fromDate);
-    let endDate = new Date(toDate);
+  // function generarFechas(fromDate:string, toDate:string) {
+  //   let fechas = [];
+  //   let currentDate = new Date(fromDate);
+  //   let endDate = new Date(toDate);
 
-    while (currentDate <= endDate) {
-        fechas.push(new Date(currentDate).toISOString().split('T')[0]);
-        currentDate.setDate(currentDate.getDate() + 1);
-    }
+  //   while (currentDate <= endDate) {
+  //       fechas.push(new Date(currentDate).toISOString().split('T')[0]);
+  //       currentDate.setDate(currentDate.getDate() + 1);
+  //   }
 
-    return fechas;
-  }
+  //   return fechas;
+  // }
 
-  function generarArrayEmpleados(diagrams:any) {
-    let agrupadoPorEmpleado:any = {};
-    diagrams.forEach((obj:any) => {
-      if (!agrupadoPorEmpleado[obj.employee_id]) {
-        agrupadoPorEmpleado[obj.employee_id] = {
-          employee_name: obj.employees.firstname + ' ' + obj.employees.lastname, // Asegúrate de que el nombre del empleado esté disponible en el objeto
-          diagrams: []
-        };
-      }
-      let fechas = generarFechas(obj.from_date, obj.to_date);
-      fechas.forEach(fecha => {
-        agrupadoPorEmpleado[obj.employee_id].diagrams.push({
-          diagram_type: obj.diagram_type,
-          date: fecha,
-        });
-      });
-    });
+  // function generarArrayEmpleados(diagrams:any) {
+  //   let agrupadoPorEmpleado:any = {};
+  //   diagrams.forEach((obj:any) => {
+  //     if (!agrupadoPorEmpleado[obj.employee_id]) {
+  //       agrupadoPorEmpleado[obj.employee_id] = {
+  //         employee_name: obj.employees.firstname + ' ' + obj.employees.lastname, // Asegúrate de que el nombre del empleado esté disponible en el objeto
+  //         diagrams: []
+  //       };
+  //     }
+  //     let fechas = generarFechas(obj.from_date, obj.to_date);
+  //     fechas.forEach(fecha => {
+  //       agrupadoPorEmpleado[obj.employee_id].diagrams.push({
+  //         diagram_type: obj.diagram_type,
+  //         date: fecha,
+  //       });
+  //     });
+  //   });
   
-    let nuevoArray = Object.keys(agrupadoPorEmpleado).map(employee_id => {
-      return {
-        employee_id: employee_id,
-        employee_name: agrupadoPorEmpleado[employee_id].employee_name,
-        diagrams: agrupadoPorEmpleado[employee_id].diagrams,
-      };
-    });
+  //   let nuevoArray = Object.keys(agrupadoPorEmpleado).map(employee_id => {
+  //     return {
+  //       employee_id: employee_id,
+  //       employee_name: agrupadoPorEmpleado[employee_id].employee_name,
+  //       diagrams: agrupadoPorEmpleado[employee_id].diagrams,
+  //     };
+  //   });
   
-    return nuevoArray;
-  }
+  //   return nuevoArray;
+  // }
 
 
-const diagramEmployees = generarArrayEmpleados(diagrams)
+// const diagramEmployees = generarArrayEmpleados(diagrams)
 
-const fechaInicio = new Date('2024/06/28');
-const fechaFin = new Date('2024/07/15');
+const fechaInicio = new Date('2024/07/28');
+const fechaFin = new Date('2024/08/15');
+
+//Esta función la voy a utilizar para realizar los filtros del componente
 
 const mes = generarDiasEntreFechas({fechaInicio, fechaFin})
 
@@ -87,12 +92,23 @@ const mes = generarDiasEntreFechas({fechaInicio, fechaFin})
       <TableHead >Empleado</TableHead>
         {mes.map((d, index) => (<TableHead key={index}>{(d.getDate()) + '/' + (d.getMonth() + 1) }</TableHead>))}
       </TableHeader>
-      <TableBody>
+      {/* <TableBody>
         {diagramEmployees.map((d:any, index:number) => (
           <TableRow key={index}>
             <TableCell>{d.employee_name}</TableCell>
             {mes.map(day => {
               const diagram = d.diagrams.find((d:any )=> new Date(d.date).toLocaleDateString() === day.toLocaleDateString())                       
+              return <TableCell key={index} className='text-center' style={{ backgroundColor: diagram?.diagram_type.color }}>{diagram?.diagram_type.short_description}</TableCell>
+            })}
+          </TableRow>
+        ))}
+      </TableBody> */}
+      <TableBody>
+        {diagrams.map((d:any, index:number) => (
+          <TableRow key={index}>
+            <TableCell>{d.employees.lastname}, {d.employees.firstname}</TableCell>
+            {mes.map(day => {
+              const diagram = diagrams.find((d:any )=> d.day === day.getDate())  
               return <TableCell key={index} className='text-center' style={{ backgroundColor: diagram?.diagram_type.color }}>{diagram?.diagram_type.short_description}</TableCell>
             })}
           </TableRow>
--- src/components/DiagramForm.tsx
+++ src/components/DiagramForm.tsx
@@ -32,30 +32,189 @@ import { z } from "zod";
 import { useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { toast } from "sonner"
-import { Card, CardContent, CardTitle } from "./ui/card"
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "./ui/card"
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "./ui/table"
 
 
 
 
 
 const datosDumy = [
-    {
-        employee: '40ebb1da-d1cc-4627-a54d-be0d63e55cf1',
-        event_diagram: '8f33d1ff-e337-4472-b9c5-0404550c07f9',
-        day: 1,
-        month: 8,
-        year: 2024
+
+  {
+
+    id: 'c47ebd53-8ad1-4234-9869-290d6abe7b45',
+    created_at: '2024-08-05T00:36:12.855782+00:00',
+    employee_id: '40ebb1da-d1cc-4627-a54d-be0d63e55cf1',
+    diagram_type: {
+      id: 'cc16fa52-f27d-4cdc-b247-4f58822ead18',
+      name: 'Trabajando Diurno',
+      color: '#d95a9a',
+      company_id: '0dd82eb6-67f9-477e-ae57-774f23c64f8c',
+      created_at: '2024-07-19T18:26:03.52746+00:00',
+      short_description: 'TD'
+    },
+    day: 1,
+    month: 8,
+    year: 2024,
+    employees: {
+      id: '40ebb1da-d1cc-4627-a54d-be0d63e55cf1',
+      city: 1628,
+      cuil: '20306456661',
+      file: '1',
+      email: 'fspiritosi@gmail.com',
+      guild: null,
+      phone: '02995810476',
+      gender: 'Masculino',
+      status: 'No avalado',
+      street: 'Los Alpatacos',
+      picture: 
+        'https://zktcbhhlcksopklpnubj.supabase.co/storage/v1/object/public/employee_photos/30645666.jpg?timestamp=1716408995938',
+      category: null,
+      lastname: 'Spiritosi',
+      province: 17,
+      covenants: null,
+      firstname: 'Fabricio Ariel',
+      is_active: true,
+      birthplace: '1f8368c4-6b0e-471f-8b04-01ee790df666',
+      company_id: '0dd82eb6-67f9-477e-ae57-774f23c64f8c',
+      created_at: '2024-05-22T20:16:34.330468+00:00',
+      nationality: 'Argentina',
+      postal_code: '8325',
+      allocated_to: [
+        '5fb4c23f-c734-492e-ad76-1b744c8c26e2', '46d8dfbb-964f-4684-9cae-bf1e0b4547a3'
+      ],
+      normal_hours: '8',
+      document_type: 'DNI',
+      street_number: '721',
+      marital_status: 'Divorciado',
+      document_number: '30645666',
+      affiliate_status: null,
+      company_position: 'CEO',
+      termination_date: null,
+      type_of_contract: 'A tiempo indeterminado',
+      workflow_diagram: 'f5d3b5f4-98aa-4656-912e-e9a3bf6479ff',
+      date_of_admission: '2024-05-22',
+      level_of_education: 'Terciario',
+      hierarchical_position: 'c2cd410d-915c-490a-87be-d7df43fe6f61',
+      reason_for_termination: null
+    }
+  },
+
+  {
+
+    id: '525507b8-58db-4072-87f4-5371da4c8f2d',
+
+    created_at: '2024-08-05T00:38:12.020096+00:00',
+
+    employee_id: '10615c85-5997-4ac4-be93-dc8e6b4bd2c0',
+
+    diagram_type: {
+
+      id: 'f684dd71-6c6d-42f7-9f8d-89c8f042a6c0',
+
+      name: 'Franco',
+
+      color: '#E92B2B',
+
+      company_id: '0dd82eb6-67f9-477e-ae57-774f23c64f8c',
+
+      created_at: '2024-07-19T18:26:28.693205+00:00',
+
+      short_description: 'F'
 
     },
-    {
-        employee: '40ebb1da-d1cc-4627-a54d-be0d63e55cf1',
-        event_diagram: '8f33d1ff-e337-4472-b9c5-0404550c07f9',
-        day: 6,
-        month: 8,
-        year: 2024
+
+    day: 1,
+
+    month: 8,
+
+    year: 2024,
+
+    employees: {
+
+      id: '10615c85-5997-4ac4-be93-dc8e6b4bd2c0',
+
+      city: 229,
+
+      cuil: '20946469492',
+
+      file: '11',
+
+      email: 'yordani12yorda@gmail.com',
+
+      guild: null,
+
+      phone: '1123807219',
+
+      gender: 'Masculino',
+
+      status: 'No avalado',
+
+      street: 'Tucuman',
+
+      picture: 
+
+        'https://zktcbhhlcksopklpnubj.supabase.co/storage/v1/object/public/employee_photos/94646949.jpg?timestamp=1716410834910',
+
+      category: null,
+
+      lastname: 'Jimenez Vasquez',
+
+      province: 2,
+
+      covenants: null,
+
+      firstname: 'Jose Jordany',
+
+      is_active: true,
+
+      birthplace: '40891050-2e81-427f-9f21-f60da2f78009',
+
+      company_id: '0dd82eb6-67f9-477e-ae57-774f23c64f8c',
+
+      created_at: '2024-05-22T20:47:12.708873+00:00',
+
+      nationality: 'Extranjero',
+
+      postal_code: '1879',
+
+      allocated_to: [ '39a3a494-828c-4b82-b9f4-1e5db61dad40' ],
+
+      normal_hours: '11',
+
+      document_type: 'DNI',
+
+      street_number: '1315',
+
+      marital_status: 'Soltero',
+
+      document_number: '94646949',
+
+      affiliate_status: null,
+
+      company_position: 'Boss',
+
+      termination_date: null,
+
+      type_of_contract: 'Período de prueba',
+
+      workflow_diagram: 'f5d3b5f4-98aa-4656-912e-e9a3bf6479ff',
+
+      date_of_admission: '2024-05-22',
+
+      level_of_education: 'Secundario',
+
+      hierarchical_position: '63289c7c-7d40-44aa-a06e-7f07905d5349',
+
+      reason_for_termination: null
 
     }
+
+  }
+
 ]
+
 
 
 
@@ -134,7 +293,7 @@ export function DiagramForm({activeEmploees, diagrams_types}:{activeEmploees:[],
         })
     }
 
-        async function onSubmit2(values: Diagram){
+    async function onSubmit2(values: Diagram){
         const data = values;
         const tipoDeDiagrama:any = diagrams_types.find((d:any) => d.id  === data.event_diagram)
         const employee:any = activeEmploees.find((e:any) => e.id === data.employee)
@@ -156,9 +315,9 @@ export function DiagramForm({activeEmploees, diagrams_types}:{activeEmploees:[],
                  year: i.getFullYear()
             }
 
-            let checkExist = datosDumy.find((d:any) => d.employee === element.employee && d.year === element.year && d.month === element.month && d.day === element.day)
+            let checkExist = datosDumy.find((d:any) => d.employee_id === element.employee && d.year === element.year && d.month === element.month && d.day === element.day)
             if(checkExist){
-                const prevEventName:any = diagrams_types.find((d:any) => d.id  === checkExist.event_diagram)
+                const prevEventName:any = diagrams_types.find((d:any) => d.id  === checkExist.diagram_type.id)
                 let errorElement = {
                     ...element,
                      prev_event: prevEventName.name
@@ -175,15 +334,7 @@ export function DiagramForm({activeEmploees, diagrams_types}:{activeEmploees:[],
         console.log('error',errorToCreate)
         setSuccesDiagrams(diagramasToCreate)
         setErrorsDiagrams(errorToCreate)
-    //    toast.promise(async () => {
-    //     const valueToSend = JSON.stringify(values)
-    //     const response = await fetch(`${URL}/api/employees/diagrams`, {method: 'POST', body:valueToSend})
-    //     return response
-    //     }, {
-    //         loading: "Cargando...",
-    //         success: `Se creo la novedad de ${tipoDeDiagrama?.name} para el empleado ${employee.full_name}`,
-    //         error: "No se pudo crear la novedad"
-    //     })
+
     }
 
     return (
@@ -332,46 +483,93 @@ export function DiagramForm({activeEmploees, diagrams_types}:{activeEmploees:[],
                         <Button className="mt-4" type="submit">Comprobar novedades</Button>
                     </form>
                 </Form>
-                </ResizablePanel>
+            </ResizablePanel>
             <ResizableHandle withHandle />
-            <ResizablePanel className="pl-6 min-w-[600px]" defaultSize={70}>
+            <ResizablePanel className="pl-6 min-w-[600px] flex flex-col gap-4" defaultSize={70}>
+                {
+                    errorsDiagrams.length > 0 && 
+                    <Card  className="bg-red-50" >
+                        <CardHeader>
+                       <CardTitle>Diagramas duplicados</CardTitle>
+                        </CardHeader>
+                        
+                        <CardContent>
+                             <Table>
+                                    <TableHeader>
+                                        <TableHead>Nombre Empleado</TableHead>
+                                        <TableHead>Fecha</TableHead>
+                                        <TableHead>Novedad Actual</TableHead>
+                                        <TableHead>Novedad Registrada</TableHead>
+                                        <TableHead></TableHead>
+                                    </TableHeader>
+                                    {errorsDiagrams.map((d:ErrorToCreate, index: number) => (
+                                        <TableBody key={index}>
+                                            <TableRow >
+                                                <TableCell>{d.employee_name}</TableCell>
+                                                <TableCell>{d.day}/{d.month}/{d.year}</TableCell>
+                                                <TableCell>{d.event_diagram_name}</TableCell>
+                                                <TableCell>{d.prev_event}</TableCell>
+                                                <TableCell className="flex gap-2 justify-around">
+                                                    <Button variant={'default'} >Actualizar</Button>
+                                                    <Button variant={'link'} className="font-bold text-red-600">Descartar</Button>
+                                                </TableCell>
+                                            </TableRow>
+                                        </TableBody>
+                                     ))}
+                            </Table>
+                        </CardContent>
+                        {
+                            errorsDiagrams.length > 1 && 
+                        <CardFooter className="flex justify-around">
+                            <Button variant={"default"}>Actualizar Todos</Button>
+                             <Button variant={'link'} className="font-bold text-red-600">Descartar Todos</Button>
+                        </CardFooter>
+                        }
+                    </Card>
+                }
                 {
                     succesDiagrams.length > 0 && 
-                    <Card className="bg-green-100">
+                    <Card className="">
+                        <CardHeader>
                         <CardTitle>Diagramas correctos</CardTitle>
+
+                        </CardHeader>
                         <CardContent>
-                            {succesDiagrams.map((d:DiagramaToCreate, index: number) => (
-                                <div key={index} className="flex gap-4">
-                                    <div>{d.employee_name}</div>
-                                    <div>{d.event_diagram_name}</div>
-                                    <div>{d.day}/{d.month}/{d.year}</div>
-                                </div>
-                            ))}
+                            <Table>
+                                    <TableHeader>
+                                        <TableHead>Nombre Empleado</TableHead>
+                                        <TableHead>Novedad</TableHead>
+                                        <TableHead>Fecha</TableHead>
+                                        <TableHead></TableHead>
+                                    </TableHeader>
+                                    {succesDiagrams.map((d:DiagramaToCreate, index: number) => (
+                                        <TableBody key={index}>
+                                            <TableRow >
+                                                <TableCell>{d.employee_name}</TableCell>
+                                                <TableCell>{d.event_diagram_name}</TableCell>
+                                                <TableCell>{d.day}/{d.month}/{d.year}</TableCell>
+                                                <TableCell className="flex gap-2 justify-around">
+                                                    <Button variant={'success'}>Crear</Button>
+                                                    <Button variant={'link'} className="font-bold text-red-600">Descartar</Button>
+                                                </TableCell>
+                                            </TableRow>
+                                        </TableBody>
+                                     ))}
+                            </Table>
                         </CardContent>
+                        {
+                            succesDiagrams.length > 1 && 
+                        <CardFooter className="flex justify-around">
+                            <Button variant={"success"}>Crear Todos</Button>
+                            <Button variant={'link'} className="font-bold text-red-600">Descartar Todos</Button>
+                        </CardFooter>
+                        }
                     </Card>
 
                 }
-                {
-                    errorsDiagrams.length > 0 && 
-                    <Card className="bg-red-100">
-                        <CardTitle>Diagramas duplicados</CardTitle>
-                        <CardContent>
-                            {errorsDiagrams.map((d:ErrorToCreate, index: number) => (
-                                <div key={index} className="flex gap-4">
-                                    <div>{d.employee_name}</div>
-                                    <div>{d.day}/{d.month}/{d.year}</div>
-                                    <div>nuevo</div>
-                                    <div>{d.event_diagram_name}</div>
-                                    <div>anterior</div>
-                                    <div>{d.prev_event}</div>
-                                    
-                                </div>
-                            ))}
-                        </CardContent>
-                    </Card>
-                }
+
             </ResizablePanel>
-    </ResizablePanelGroup>
+        </ResizablePanelGroup>
 
     )
 }
