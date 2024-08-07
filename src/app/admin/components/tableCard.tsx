f03b5daa94c8322d2d03c24c737aa245b2af8bbe
Fabricio Spiritosi <fspiritosi@gmail.com>
2024-08-07 12:08:38 -0300

    resolviendo tabla de vista de diagramas

--- src/app/admin/components/tableCard.tsx
+++ src/app/admin/components/tableCard.tsx
@@ -28,7 +28,7 @@ export default function CardTable({ title, data, dbName }: Props) {
       estado: dato.is_active ? 'Activo' : 'Inactivo',
       nombre: dato.name,
     }));
-    console.log(dataToDownload);
+
     const worksheet = XLSX.utils.json_to_sheet(dataToDownload);
     const workbook = XLSX.utils.book_new();
     XLSX.utils.book_append_sheet(workbook, worksheet, 'Dates');
@@ -39,9 +39,6 @@ export default function CardTable({ title, data, dbName }: Props) {
     <Card className="md:min-w-[600px]">
       <CardHeader>
         <CardTitle>{title}</CardTitle>
-        {/* <CardDescription>
-        Crea, edita o elimina los distintos tipos de elementos que tendrán disponibles los usuarios
-        </CardDescription> */}
       </CardHeader>
       <CardContent>
         <Table>
@@ -88,92 +85,8 @@ export default function CardTable({ title, data, dbName }: Props) {
             ))}
           </TableBody>
         </Table>
-        {/* <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead className="w-[100px]">SKU</TableHead>
-              <TableHead>Stock</TableHead>
-              <TableHead>Price</TableHead>
-              <TableHead className="w-[100px]">Size</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            <TableRow>
-              <TableCell className="font-semibold">GGPC-001</TableCell>
-              <TableCell>
-                <Label htmlFor="stock-1" className="sr-only">
-                  Stock
-                </Label>
-                <Input id="stock-1" type="number" defaultValue="100" />
-              </TableCell>
-              <TableCell>
-                <Label htmlFor="price-1" className="sr-only">
-                  Price
-                </Label>
-                <Input id="price-1" type="number" defaultValue="99.99" />
-              </TableCell>
-              <TableCell>
-                <ToggleGroup type="single" defaultValue="s" variant="outline">
-                  <ToggleGroupItem value="s">S</ToggleGroupItem>
-                  <ToggleGroupItem value="m">M</ToggleGroupItem>
-                  <ToggleGroupItem value="l">L</ToggleGroupItem>
-                </ToggleGroup>
-              </TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell className="font-semibold">GGPC-002</TableCell>
-              <TableCell>
-                <Label htmlFor="stock-2" className="sr-only">
-                  Stock
-                </Label>
-                <Input id="stock-2" type="number" defaultValue="143" />
-              </TableCell>
-              <TableCell>
-                <Label htmlFor="price-2" className="sr-only">
-                  Price
-                </Label>
-                <Input id="price-2" type="number" defaultValue="99.99" />
-              </TableCell>
-              <TableCell>
-                <ToggleGroup type="single" defaultValue="m" variant="outline">
-                  <ToggleGroupItem value="s">S</ToggleGroupItem>
-                  <ToggleGroupItem value="m">M</ToggleGroupItem>
-                  <ToggleGroupItem value="l">L</ToggleGroupItem>
-                </ToggleGroup>
-              </TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell className="font-semibold">GGPC-003</TableCell>
-              <TableCell>
-                <Label htmlFor="stock-3" className="sr-only">
-                  Stock
-                </Label>
-                <Input id="stock-3" type="number" defaultValue="32" />
-              </TableCell>
-              <TableCell>
-                <Label htmlFor="price-3" className="sr-only">
-                  Stock
-                </Label>
-                <Input id="price-3" type="number" defaultValue="99.99" />
-              </TableCell>
-              <TableCell>
-                <ToggleGroup type="single" defaultValue="s" variant="outline">
-                  <ToggleGroupItem value="s">S</ToggleGroupItem>
-                  <ToggleGroupItem value="m">M</ToggleGroupItem>
-                  <ToggleGroupItem value="l">L</ToggleGroupItem>
-                </ToggleGroup>
-              </TableCell>
-            </TableRow>
-          </TableBody>
-        </Table> */}
       </CardContent>
-      <CardFooter className="justify-center border-t p-4">
-        {/* <CreateDialog title={title} dbName={dbName} /> */}
-        {/* <Button size="sm" variant="ghost" className="gap-1">
-          <PlusCircle className="h-3.5 w-3.5" />
-          Agregar {title}
-        </Button> */}
-      </CardFooter>
+      <CardFooter className="justify-center border-t p-4"></CardFooter>
       <Button onClick={() => createAndDownloadFile(data)}>Descargar Data</Button>
     </Card>
   );
--- src/components/Diagrams/DiagramEmployeeView.tsx
+++ src/components/Diagrams/DiagramEmployeeView.tsx
@@ -1,218 +1,285 @@
-"use client"
-import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table"
-import { CalendarIcon } from "@radix-ui/react-icons"
-import { addDays, format } from "date-fns"
+'use client';
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
+import { CalendarIcon, CaretSortIcon, CheckIcon } from '@radix-ui/react-icons';
+import { addDays, format } from 'date-fns';
 import { es } from 'date-fns/locale';
-import { DateRange } from "react-day-picker"
- 
-import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
-import { Calendar } from "@/components/ui/calendar"
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover"
-import { useState } from "react"
+import { DateRange } from 'react-day-picker';
 
+import { Button } from '@/components/ui/button';
+import { Calendar } from '@/components/ui/calendar';
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
+import { cn } from '@/lib/utils';
+import { useEffect, useState } from 'react';
 
-import {
-  DropdownMenu,
-  DropdownMenuCheckboxItem,
-  DropdownMenuContent,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import { DropdownMenuCheckboxItemProps } from "@radix-ui/react-dropdown-menu"
+import { zodResolver } from '@hookform/resolvers/zod';
+import { DropdownMenuCheckboxItemProps } from '@radix-ui/react-dropdown-menu';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem } from '../ui/command';
+import { Form, FormControl, FormDescription, FormField, FormItem, FormMessage } from '../ui/form';
 
-
-type Checked = DropdownMenuCheckboxItemProps["checked"]
+type Checked = DropdownMenuCheckboxItemProps['checked'];
 type DiamgramParsed = {
-  name: string,
-  lastName: string,
-  diagram_type: string,
-  date: string
-}
+  name: string;
+  lastName: string;
+  diagram_type: string;
+  date: string;
+};
 
-function DiagramEmployeeView({diagrams, activeEmployees,  className}:{diagrams:any, activeEmployees:any, className?:React.HTMLAttributes<HTMLDivElement>}) {
-
-  console.log(diagrams)
+function DiagramEmployeeView({
+  diagrams,
+  activeEmployees,
+  className,
+}: {
+  diagrams: any;
+  activeEmployees: any;
+  className?: React.HTMLAttributes<HTMLDivElement>;
+}) {
   const [date, setDate] = useState<DateRange | undefined>({
     from: new Date(),
     to: addDays(new Date(), 30),
-  })
-  const [diagramsToShow, setDiagramsToShow] = useState<{}[]>([])
-  const [showStatusBar, setShowStatusBar] = useState<Checked>(true)
-  const [selectEmployees, setSelectEmployees] = useState<string[]>([]);
+  });
+  const [selectedResources, setSelectedResources] = useState<string[]>([]);
+  const [filteredResources, setFilteredResources] = useState(activeEmployees);
+  const [inputValue, setInputValue] = useState<string>('');
 
   const fechaInicio = date?.from;
   const fechaFin = date?.to;
 
-  function generarDiasEntreFechas({fechaInicio, fechaFin}:{fechaInicio?:Date, fechaFin?:Date}) {
-      const dias = [];
-      let fechaActual = new Date(fechaInicio!);
-    
-      while (fechaActual <= new Date(fechaFin!)) {
-        dias.push(new Date(fechaActual));
-        fechaActual.setDate(fechaActual.getDate() + 1);
-      }
-    
-      return dias;
+  /*---------------------INICIO ESQUEMA EMPLEADOS---------------------------*/
+  const formSchema = z.object({
+    resources: z
+      .array(z.string(), { required_error: 'Los recursos son requeridos' })
+      .min(1, 'Selecciona al menos 1 recursos'),
+    //! Cambiar a 1 si se necesita que sea solo uno
+  });
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      resources: [],
+    },
+  });
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    const resourceId = selectedResources?.map((resource) => {
+      const employee = activeEmployees.find((element: any) => {
+        return element.document === resource;
+      });
+      return setSelectedResources(employee?.id);
+    });
   }
-  
-  function filtrarEmpleados (id:string) {
-    if(selectEmployees.length === 0) {
-      selectEmployees.push(id)
+
+  /*---------------------FIN ESQUEMA EMPLEADOS------------------------------*/
+
+  function generarDiasEntreFechas({ fechaInicio, fechaFin }: { fechaInicio?: Date; fechaFin?: Date }) {
+    const dias = [];
+    let fechaActual = new Date(fechaInicio!);
+
+    while (fechaActual <= new Date(fechaFin!)) {
+      dias.push(new Date(fechaActual));
+      fechaActual.setDate(fechaActual.getDate() + 1);
     }
-    if(selectEmployees.includes(id)){
-      setSelectEmployees(selectEmployees.filter(em => em != id))
-    }else[
-      selectEmployees.push(id)
-    ]
+
+    return dias;
   }
 
-const groupedDiagrams = diagrams.reduce((acc:any, diagram:any) => {
-  if (!acc[diagram.employee_id]) {
-    acc[diagram.employee_id] = [];
-  }
-  acc[diagram.employee_id].push(diagram);
-  return acc;
-}, {});
+  const groupedDiagrams = diagrams.reduce((acc: any, diagram: any) => {
+    if (!acc[diagram.employee_id]) {
+      acc[diagram.employee_id] = [];
+    }
+    acc[diagram.employee_id].push(diagram);
+    return acc;
+  }, {});
 
+  const mes = generarDiasEntreFechas({ fechaInicio, fechaFin });
 
+  console.log(filteredResources);
+  console.log(selectedResources);
 
-const mes = generarDiasEntreFechas({fechaInicio,fechaFin})
-
-const filteredDiagrams = () => {
-  if (fechaInicio && fechaFin){
-    setDiagramsToShow(diagrams.filter((d:any) => {d.day >= fechaInicio && d.day <= fechaFin})) 
-    console.log(diagramsToShow)
-  }
-}
-
-
+  useEffect(() => {
+    form.reset();
+    setSelectedResources([]);
+  }, [activeEmployees]);
 
   return (
     <div>
       <div className="py-2 w-full flex justify-around">
-      <>
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="outline">Selecciona empleados</Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent className="w-full">
-            <DropdownMenuLabel>Empleados</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            {activeEmployees.map((e:any) => (
-              <DropdownMenuCheckboxItem
-              key={e.id}
-              checked={showStatusBar}
-              onCheckedChange={setShowStatusBar}  
-            >
-            {e.full_name}
-            </DropdownMenuCheckboxItem>
-            ))}
-            
-          </DropdownMenuContent>
-        </DropdownMenu>
-      </>
-      <div className={cn("grid gap-2", className)}>
-      <Popover>
-        <PopoverTrigger asChild>
-          <Button
-            id="date"
-            variant={"outline"}
-            className={cn(
-              "w-[300px] justify-start text-left font-normal",
-              !date && "text-muted-foreground"
-            )}
-          >
-            <CalendarIcon className="mr-2 h-4 w-4" />
-            {date?.from ? (
-              date.to ? (
-                <>
-                  {format(date.from, "dd/MM/yyyyy", { locale: es })} -{" "}
-                  {format(date.to, "dd/MM/yyyyy", { locale: es })}
-                </>
-              ) : (
-                format(date.from, "dd/MM/yyyyy", { locale: es })
-              )
-            ) : (
-              <span>Pick a date</span>
-            )}
-          </Button>
-        </PopoverTrigger>
-        <PopoverContent className="w-auto p-0" align="start">
-          <Calendar
-            initialFocus
-            mode="range"
-            defaultMonth={date?.from}
-            selected={date}
-            onSelect={setDate}
-            numberOfMonths={2}
-          />
-        </PopoverContent>
-      </Popover>
-      </div>
-      <Button onClick={filteredDiagrams}>Mostrar diagramas</Button>
+        <>
+          <div>
+            <Form {...form}>
+              <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                <FormField
+                  control={form.control}
+                  name="resources"
+                  render={({ field }) => (
+                    <FormItem className="flex flex-col">
+                      <Popover>
+                        <PopoverTrigger asChild>
+                          <FormControl>
+                            <Button
+                              variant="outline"
+                              role="combobox"
+                              className={cn('justify-between', !field.value && 'text-muted-foreground')}
+                            >
+                              {`${selectedResources.length || '0'} empleados seleccionados`}
+                              <CaretSortIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                            </Button>
+                          </FormControl>
+                        </PopoverTrigger>
+                        <PopoverContent className=" p-0">
+                          <Command>
+                            <CommandInput
+                              placeholder="Buscar recursos..."
+                              className="h-9"
+                              onFocus={() => {
+                                setFilteredResources(activeEmployees);
+                              }}
+                              onInput={(e) => {
+                                const inputValue = (e.target as HTMLInputElement).value.toLowerCase();
+                                setInputValue(inputValue);
+                                const isNumberInput = /^\d+$/.test(inputValue);
+
+                                const filteredresources = activeEmployees?.filter((person: any) => {
+                                  if (isNumberInput) {
+                                    return person.document.includes(inputValue);
+                                  } else {
+                                    return (
+                                      person.name.toLowerCase().includes(inputValue) ||
+                                      person.document.includes(inputValue)
+                                    );
+                                  }
+                                });
+                                setFilteredResources(filteredresources);
+                              }}
+                            />
+                            <CommandEmpty>No se encontraron recursos con ese nombre o documento</CommandEmpty>
+                            <CommandGroup>
+                              {filteredResources?.map((person: any) => {
+                                //const key = /^\d+$/.test(inputValue) ? person.document : person.name;
+                                const key = /^\d+$/.test(inputValue) ? person.id : person.full_name;
+
+                                //const value = /^\d+$/.test(inputValue) ? person.document : person.name;
+                                const value = /^\d+$/.test(inputValue) ? person.id : person.full_name;
+
+                                return (
+                                  <CommandItem
+                                    value={value}
+                                    key={key}
+                                    onSelect={() => {
+                                      const updatedResources = selectedResources.includes(person.id)
+                                        ? selectedResources?.filter((resource) => resource !== person.id)
+                                        : [...selectedResources, person.id];
+
+                                      setSelectedResources(updatedResources);
+                                      form.setValue('resources', updatedResources);
+                                    }}
+                                  >
+                                    {person.full_name}
+                                    <CheckIcon
+                                      className={cn(
+                                        'ml-auto h-4 w-4',
+                                        selectedResources?.includes(person.id) ? 'opacity-100' : 'opacity-0'
+                                      )}
+                                    />
+                                  </CommandItem>
+                                );
+                              })}
+                            </CommandGroup>
+                          </Command>
+                        </PopoverContent>
+                      </Popover>
+                      <FormDescription>{` Selecciona al menos 1 recurso para ver su diagrama.`}</FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </form>
+            </Form>
+          </div>
+        </>
+        <div className={cn('grid gap-2', className)}>
+          <Popover>
+            <PopoverTrigger asChild>
+              <Button
+                id="date"
+                variant={'outline'}
+                className={cn('w-[300px] justify-start text-left font-normal', !date && 'text-muted-foreground')}
+              >
+                <CalendarIcon className="mr-2 h-4 w-4" />
+                {date?.from ? (
+                  date.to ? (
+                    <>
+                      {format(date.from, 'dd/MM/yyyyy', { locale: es })} -{' '}
+                      {format(date.to, 'dd/MM/yyyyy', { locale: es })}
+                    </>
+                  ) : (
+                    format(date.from, 'dd/MM/yyyyy', { locale: es })
+                  )
+                ) : (
+                  <span>Pick a date</span>
+                )}
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-auto p-0" align="start">
+              <Calendar
+                initialFocus
+                mode="range"
+                defaultMonth={date?.from}
+                selected={date}
+                onSelect={setDate}
+                numberOfMonths={2}
+              />
+            </PopoverContent>
+          </Popover>
+        </div>
       </div>
       <Table>
         <TableHeader>
-        <TableHead >Empleado</TableHead>
-          {mes.map((d, index) => (<TableHead key={index} className="text-center">{(d.getDate()) + '/' + (d.getMonth() + 1) }</TableHead>))}
+          <TableHead>Empleado</TableHead>
+          {mes.map((d, index) => (
+            <TableHead key={index} className="text-center">
+              {d.getDate() + '/' + (d.getMonth() + 1)}
+            </TableHead>
+          ))}
         </TableHeader>
 
         <TableBody>
-
-            {Object.keys(groupedDiagrams).map((employeeId, index) => {
-  const employeeDiagrams = groupedDiagrams[employeeId];
-  const employee = employeeDiagrams[0].employees; // Asumimos que todos los diagramas tienen el mismo empleado
-
-  return (
-    <TableRow key={index}>
-      <TableCell>{employee.lastname}, {employee.firstname}</TableCell>
-      {mes.map((day, dayIndex) => {
-        const diagram = employeeDiagrams.find((diagram:any) => 
-          diagram.day === day.getDate() && 
-          diagram.month === (day.getMonth() + 1) && 
-          diagram.year === day.getFullYear()
-        );
-        return (
-          <TableCell 
-            key={dayIndex} 
-            className='text-center border' 
-            style={{ backgroundColor: diagram?.diagram_type.color }}
-          >
-            {diagram?.diagram_type.short_description}
-          </TableCell>
-        );
-      })}
-    </TableRow>
-  );
-})}
-
+          {selectedResources.length > 0 &&
+            Object.keys(groupedDiagrams)
+              .filter((employeeId) => selectedResources.includes(employeeId))
+              .map((employeeId, index) => {
+                const employeeDiagrams = groupedDiagrams[employeeId];
+                const employee = employeeDiagrams[0].employees; // Asumimos que todos los diagramas tienen el mismo empleado
+                return (
+                  <TableRow key={index}>
+                    <TableCell>
+                      {employee.lastname}, {employee.firstname}
+                    </TableCell>
+                    {mes.map((day, dayIndex) => {
+                      const diagram = employeeDiagrams.find(
+                        (diagram: any) =>
+                          diagram.day === day.getDate() &&
+                          diagram.month === day.getMonth() + 1 &&
+                          diagram.year === day.getFullYear()
+                      );
+                      return (
+                        <TableCell
+                          key={dayIndex}
+                          className="text-center border"
+                          style={{ backgroundColor: diagram?.diagram_type.color }}
+                        >
+                          {diagram?.diagram_type.short_description}
+                        </TableCell>
+                      );
+                    })}
+                  </TableRow>
+                );
+              })}
         </TableBody>
       </Table>
-      
     </div>
-  )
+  );
 }
 
-export default DiagramEmployeeView
-
-
-
-
-
-
-
-
+export default DiagramEmployeeView;
