17b31232d73b25bb51af479f1de231c536a79944
Yordan <yordanpz@hotmail.com>
2024-08-09 09:50:27 -0400

    Arreglando tabla de equipos

--- src/app/api/equipment/route.ts
+++ src/app/api/equipment/route.ts
@@ -5,21 +5,19 @@ export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
   const company_id = searchParams.get('actual');
   const user_id = searchParams.get('user');
- 
+
   try {
     let { data: equipments, error } = await supabase
       .from('vehicles')
-      .select('*')
+      .select('*, brand_vehicles(name), model_vehicles(name),types_of_vehicles(name),type(name)')
       // Filters
       .eq('company_id', company_id);
 
     const data = equipments;
-    
+
     if (error) {
       throw new Error(JSON.stringify(error));
     }
     return Response.json({ data });
-  } catch (error) {
-    
-  }
+  } catch (error) {}
 }
