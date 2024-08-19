48e3b671fca5f1a33ae61033f00bf7560f2d852c
Fabricio Spiritosi <fspiritosi@gmail.com>
2024-08-19 09:40:51 -0300

    Modificando el fetch de los items

--- src/lib/utils/geters.ts
+++ src/lib/utils/geters.ts
@@ -13,11 +13,11 @@ export async function getTotalResourses(){
   const coockiesStore = cookies();
   const company_id = coockiesStore.get('actualComp')?.value;
   const { employees } = await fetch(`${URL}/api/employees?actual=${company_id}&user=${user?.id}`).then((e) => e.json());
-  const { data } = await fetch(`${URL}/api/equipment?actual=${company_id}&user=${user?.id}`).then((e) => e.json());
+  const { data:vehicles } = await fetch(`${URL}/api/equipment?actual=${company_id}&user=${user?.id}`).then((e) => e.json());
 
   return {
-    totalResourses: employees.length + data.length,
+    totalResourses: employees.length + vehicles.length,
     employees: employees.length,
-    vehicles: data.length,
+    vehicles: vehicles.length,
   };
 }
