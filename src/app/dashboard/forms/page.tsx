9d5d8a9e5321a64e718ea9d329b149bfce925f86
Yordani Jimenez <yordanpz@hotmail.com>
2024-08-23 13:22:36 -0400

    Cambio de ruta

---
+++ src/app/dashboard/forms/page.tsx
@@ -0,0 +1,51 @@
+import Viewcomponent from '@/components/ViewComponent';
+import CreatedForm from './components/CreatedForm';
+import NewForm from './components/NewForm';
+function MantenimientoPage() {
+  const viewData = {
+    defaultValue: 'show_created_forms',
+    tabsValues: [
+      {
+        value: 'show_created_forms',
+        name: 'Creados',
+        restricted: [''],
+        content: {
+          title: 'Formularios creados',
+          description: 'Aquí encontrarás todos los formularios creados',
+          buttonActioRestricted: [''],
+          component: <CreatedForm />,
+        },
+      },
+      {
+        value: 'create_new_form',
+        name: 'Crear nuevo formulario',
+        restricted: [''],
+        content: {
+          title: 'Crear nuevo formulario',
+          description: 'Aquí podrás crear un nuevo formulario',
+          buttonActioRestricted: [''],
+          component: <NewForm />,
+        },
+      },
+      // {
+      //   value: 'Cargados',
+      //   name: 'Formularios cargados',
+      //   restricted: [''],
+      //   content: {
+      //     title: 'Formularios cargados',
+      //     description: 'Aquí encontrarás todos los formularios cargados',
+      //     buttonActioRestricted: [''],
+      //     component: <FormCustomContainer showAnswers={true} employees={true} company={true}  documents={true} equipment={true} />,
+      //   },
+      // },
+    ],
+  };
+
+  return (
+    <div className="h-full">
+      <Viewcomponent viewData={viewData} />
+    </div>
+  );
+}
+
+export default MantenimientoPage;
