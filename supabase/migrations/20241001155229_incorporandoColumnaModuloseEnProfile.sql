a6f66879931e026f611065b51ba288c7df6a4d33
Fabricio Spiritosi <fspiritosi@gmail.com>
2024-10-01 14:02:57 -0300

    asd

--- supabase/migrations/20241001155229_incorporandoColumnaModuloseEnProfile.sql
+++ supabase/migrations/20241001155229_incorporandoColumnaModuloseEnProfile.sql
@@ -1,8 +1,8 @@
 create type "public"."modulos" as enum ('empresa', 'empleados', 'equipos', 'documentación', 'mantenimiento', 'dashboard', 'ayuda');
 
-alter table "public"."diagram_type" drop constraint "diagram_type_name_key";
+-- alter table "public"."diagram_type" drop constraint "diagram_type_name_key";
 
-drop index if exists "public"."diagram_type_name_key";
+-- drop index if exists "public"."diagram_type_name_key";
 
 alter table "public"."profile" add column "modulos" modulos[];
 
