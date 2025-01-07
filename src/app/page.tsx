55a82fc6a5857ec22e5919df54b8cbc3ba36cfd6
fspiritosi <fspiritosi@gmail.com>
2025-01-06 23:28:36 -0300

    ajustando title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 import Landing from '@/components/landing_components/Landing';
-import { Metadata } from 'next';
 
-export const metadata: Metadata = {
-  title: 'Free Next.js Template for Startup and SaaS',
-  description: 'This is Home for Startup Nextjs Template',
-  // other metadata
-};
+// export const metadata: Metadata = {
+//   title: 'Free Next.js Template for Startup and SaaS',
+//   description: 'This is Home for Startup Nextjs Template',
+//   // other metadata
+// };
 
 export default function Home() {
   return <Landing />;
