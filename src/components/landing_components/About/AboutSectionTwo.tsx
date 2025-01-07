7b68224c005291efa359d892a2824a4d59779dfb
fspiritosi <fspiritosi@gmail.com>
2025-01-06 23:33:34 -0300

    modificando imagen de saction two

--- src/components/landing_components/About/AboutSectionTwo.tsx
+++ src/components/landing_components/About/AboutSectionTwo.tsx
@@ -11,17 +11,13 @@ const AboutSectionTwo = () => {
               className="relative mx-auto mb-12 aspect-[25/24] max-w-[500px] text-center lg:m-0"
               data-wow-delay=".15s"
             >
-              <Image
-                src={empleados}
-                alt="about image"
-                className="drop-shadow-three dark:hidden dark:drop-shadow-none"
-              />
-              <Image
+              <Image src={empleados} alt="about image" className="drop-shadow-three dark:drop-shadow-none" />
+              {/* <Image
                 src="/images/about/about-image-2-dark.svg"
                 alt="about image"
                 fill
                 className="hidden drop-shadow-three dark:block dark:drop-shadow-none"
-              />
+              /> */}
             </div>
           </div>
           <div className="w-full px-4 lg:w-1/2">
